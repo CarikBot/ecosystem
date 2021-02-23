474d0794c219bda75a1214c69821fc571140ae55
Luri <luri@kioss.com>
2021-02-23 11:20:32 +0700

    initial config file

---
+++ source/services/config_template.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* File: config.php
+* Configuration File
+*
+* @date       20-09-2017 22:33
+* @category   AksiIDE
+* @package    AksiIDE
+* @subpackage
+* @copyright  Copyright (c) 2013-endless AksiIDE
+* @license
+* @version
+* @link       http://www.aksiide.com
+* @since
+*/
+
+date_default_timezone_set('Asia/Jakarta');
+$Config = [
+  "packages" => [
+    "main" => [
+      "CarikBot" => [
+        "token" => "your_token"
+      ]
+    ],
+    "tools" => [
+      "echo" => [
+        "token" => "your_token"
+      ]
+    ]
+  ]
+];
