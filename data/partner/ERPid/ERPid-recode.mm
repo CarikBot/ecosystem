8dad1f041f655e00339a1319b167105c1c0f77e2
Carik Ecosystem Builder <carik.bot@gmail.com>
2021-02-28 22:49:30 +0700

    Ecosystem Builder: partner.ERPid

--- data/partner/ERPid/ERPid-recode.mm
+++ data/partner/ERPid/ERPid-recode.mm
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <map version="0.8.1">
-  <node CREATED="1614095744512" ID="b9aa22deba98b3b20c7ac8aca2" MODIFIED="1614095744512" STYLE="bubble" TEXT="ERPid">
+  <node CREATED="1614527250798" ID="b9aa22deba98b3b20c7ac8aca2" MODIFIED="1614527250798" STYLE="bubble" TEXT="ERPid">
     <hook NAME="accessories/plugins/NodeNote.properties">
       <text>*ERP.id*
 
@@ -14,47 +14,47 @@ We provide ERP System for your all your business processes in one place.
 erp.id
 </text>
     </hook>
-    <node CREATED="1614095744512" ID="193b56735e689ae86a01d91513" MODIFIED="1614095744512" POSITION="right" TEXT="🌟 Product">
-      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614095744512" FOLDER="true" ID="61lrimrpaj3e6ejcmn8shl9n9u" MODIFIED="1614095744512" TEXT="*ERP.id Product*&#10;&#10;We provide:&#10;&#10;Planning Management System.&#10;PMS for help plan, organize, and manage resource tools and develop resource estimates.&#10;&#10;Project Management System.&#10;To manage all projects in a company, schedules and get interaction to all actors to.&#10;&#10;Knowledge Management&#10;Process of creating, sharing, using and managing the knowledge and information of an organisation.&#10;&#10;Customer Relationship Management&#10;Manage a company's interaction with current and potential customers.&#10;&#10;Supervision Management System&#10;The action of overseeing and managing employees in the workplace, and make effectively manage the work force in a company.&#10;&#10;Manufacturer&#10;There are many variations of passages of lorem Ipsum but majority have suffered.">
+    <node CREATED="1614527250799" ID="193b56735e689ae86a01d91513" MODIFIED="1614527250799" POSITION="right" TEXT="🌟 Product">
+      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614527250799" FOLDER="true" ID="61lrimrpaj3e6ejcmn8shl9n9u" MODIFIED="1614527250799" TEXT="*ERP.id Product*&#10;&#10;We provide:&#10;&#10;Planning Management System.&#10;PMS for help plan, organize, and manage resource tools and develop resource estimates.&#10;&#10;Project Management System.&#10;To manage all projects in a company, schedules and get interaction to all actors to.&#10;&#10;Knowledge Management&#10;Process of creating, sharing, using and managing the knowledge and information of an organisation.&#10;&#10;Customer Relationship Management&#10;Manage a company's interaction with current and potential customers.&#10;&#10;Supervision Management System&#10;The action of overseeing and managing employees in the workplace, and make effectively manage the work force in a company.&#10;&#10;Manufacturer&#10;There are many variations of passages of lorem Ipsum but majority have suffered.">
         <font NAME="SansSerif" SIZE="10"/>
       </node>
-      <node CREATED="1614095744512" FOLDER="true" ID="34v7r318aenr666fpq01cugfnf" MODIFIED="1614095744512" TEXT="Action Type">
-        <node CREATED="1614095744512" FOLDER="true" ID="4ie4bu0071d822mts7q7l176ss" MODIFIED="1614095744512" TEXT="button"/>
+      <node CREATED="1614527250799" FOLDER="true" ID="34v7r318aenr666fpq01cugfnf" MODIFIED="1614527250799" TEXT="Action Type">
+        <node CREATED="1614527250800" FOLDER="true" ID="4ie4bu0071d822mts7q7l176ss" MODIFIED="1614527250800" TEXT="button"/>
       </node>
-      <node CREATED="1614095744512" ID="35gt00dgj7bmqb86f0gbkn5i4v" MODIFIED="1614095744512" TEXT="Action">
-        <node CREATED="1614095744512" FOLDER="true" ID="87ce8e4d-5528-4c4f-a50c-08dd4c674514" MODIFIED="1614095744512" TEXT="📞 Contact Us">
+      <node CREATED="1614527250800" ID="35gt00dgj7bmqb86f0gbkn5i4v" MODIFIED="1614527250800" TEXT="Action">
+        <node CREATED="1614527250800" FOLDER="true" ID="87ce8e4d-5528-4c4f-a50c-08dd4c674514" MODIFIED="1614527250800" TEXT="📞 Contact Us">
           <hook NAME="accessories/plugins/NodeNote.properties">
             <text>callback:contact erpid</text>
           </hook>
         </node>
       </node>
-      <node CREATED="1614095744512" ID="31igbk16rdqgimdj9akg3caiod" MODIFIED="1614095744512" TEXT="pattern">
-        <node CREATED="1614095744512" ID="0pb40hr3440vfosp4jib0kogik" MODIFIED="1614095744512" TEXT="product (erp|erpid)"/>
+      <node CREATED="1614527250800" ID="31igbk16rdqgimdj9akg3caiod" MODIFIED="1614527250800" TEXT="pattern">
+        <node CREATED="1614527250800" ID="0pb40hr3440vfosp4jib0kogik" MODIFIED="1614527250800" TEXT="product (erp|erpid)"/>
       </node>
     </node>
-    <node CREATED="1614095744512" ID="624a5ab8-e8c1-4d73-bd22-d59ba7a75861" MODIFIED="1614095744512" POSITION="right" TEXT="🗓 Portfolio">
-      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614095744512" FOLDER="true" ID="55k5a46vmbk928ppfo4vhhn2pr" MODIFIED="1614095744512" TEXT="*ERP.id Portfolio*[.](https://erp.id/ui/default/assets/frontend/img/feature/app-img4.png)&#10;&#10;[See our portfolio on our site](https://erp.id/#portofolio)">
+    <node CREATED="1614527250800" ID="624a5ab8-e8c1-4d73-bd22-d59ba7a75861" MODIFIED="1614527250800" POSITION="right" TEXT="🗓 Portfolio">
+      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614527250800" FOLDER="true" ID="55k5a46vmbk928ppfo4vhhn2pr" MODIFIED="1614527250800" TEXT="*ERP.id Portfolio*[.](https://erp.id/ui/default/assets/frontend/img/feature/app-img4.png)&#10;&#10;[See our portfolio on our site](https://erp.id/#portofolio)">
         <font NAME="SansSerif" SIZE="10"/>
       </node>
-      <node CREATED="1614095744512" ID="24au3hr7l8ki421gfj2frn3tpa" MODIFIED="1614095744512" TEXT="Action Type">
-        <node CREATED="1614095744512" FOLDER="true" ID="2inqavij1o2685mrdtgo1enp9o" MODIFIED="1614095744512" TEXT="button"/>
+      <node CREATED="1614527250800" ID="24au3hr7l8ki421gfj2frn3tpa" MODIFIED="1614527250800" TEXT="Action Type">
+        <node CREATED="1614527250800" FOLDER="true" ID="2inqavij1o2685mrdtgo1enp9o" MODIFIED="1614527250800" TEXT="button"/>
       </node>
-      <node CREATED="1614095744512" ID="0584p8cnsragglitoardpb8t18" MODIFIED="1614095744512" TEXT="Action">
-        <node CREATED="1614095744512" FOLDER="true" ID="439ed0c9m2p1amnoau3nm255v7" MODIFIED="1614095744512" TEXT="📞 Contact Us">
+      <node CREATED="1614527250800" ID="0584p8cnsragglitoardpb8t18" MODIFIED="1614527250800" TEXT="Action">
+        <node CREATED="1614527250800" FOLDER="true" ID="439ed0c9m2p1amnoau3nm255v7" MODIFIED="1614527250800" TEXT="📞 Contact Us">
           <hook NAME="accessories/plugins/NodeNote.properties">
             <text>callback:contact erpid</text>
           </hook>
         </node>
       </node>
-      <node CREATED="1614095744512" ID="4q7vbole2698m8turrvao8ctcq" MODIFIED="1614095744512" TEXT="pattern">
-        <node CREATED="1614095744512" FOLDER="true" ID="0tgnb1vhg8jie6in3ht2qbq6eg" MODIFIED="1614095744512" TEXT="portfolio (erpid|erp)"/>
+      <node CREATED="1614527250800" ID="4q7vbole2698m8turrvao8ctcq" MODIFIED="1614527250800" TEXT="pattern">
+        <node CREATED="1614527250800" FOLDER="true" ID="0tgnb1vhg8jie6in3ht2qbq6eg" MODIFIED="1614527250800" TEXT="portfolio (erpid|erp)"/>
       </node>
     </node>
-    <node CREATED="1614095744512" ID="67ddbcb1-85c9-4478-a0aa-580e9fdcd971" MODIFIED="1614095744512" POSITION="left" TEXT="📞 Contact">
-      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614095744512" FOLDER="true" ID="1m6eo1b0qeurhkrr3m8mf4ggan" MODIFIED="1614095744512" TEXT="*Contact ERP Indonesia*[.](https://erp.id/ui/default/assets/frontend/img/feature/user-interact.png)&#10;&#10;+62-817-0332-1842&#10;[Whatsapp](wa.me/628170332-842)&#10;Email: halo@erp.id&#10;&#10;*About Us*&#10;ERP System that provides an integrated overview of the company's operations into a system that integrates and optimizes business processes, and provides business information from all entities in all branch office locations."/>
-      <node CREATED="1614095744512" ID="23m45snsmllgs3pnmpnsvu5rf3" MODIFIED="1614095744512" TEXT="pattern">
-        <node CREATED="1614095744512" FOLDER="true" ID="6vt3fgv7ihg1b5b7vlaua3pg6n" MODIFIED="1614095744512" TEXT="(contact|kontak) (erpid|erp)"/>
-        <node CREATED="1614095744512" ID="4ilflt15tnjv0b1vcf28uf7q6k" MODIFIED="1614095744512" TEXT="(erpid|erp) (contact|kontak)"/>
+    <node CREATED="1614527250800" ID="67ddbcb1-85c9-4478-a0aa-580e9fdcd971" MODIFIED="1614527250800" POSITION="left" TEXT="📞 Contact">
+      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614527250800" FOLDER="true" ID="1m6eo1b0qeurhkrr3m8mf4ggan" MODIFIED="1614527250800" TEXT="*Contact ERP Indonesia*[.](https://erp.id/ui/default/assets/frontend/img/feature/user-interact.png)&#10;&#10;+62-817-0332-1842&#10;[Whatsapp](wa.me/628170332842)&#10;Email: halo@erp.id&#10;&#10;*About Us*&#10;ERP System that provides an integrated overview of the company's operations into a system that integrates and optimizes business processes, and provides business information from all entities in all branch office locations."/>
+      <node CREATED="1614527250800" ID="23m45snsmllgs3pnmpnsvu5rf3" MODIFIED="1614527250800" TEXT="pattern">
+        <node CREATED="1614527250800" FOLDER="true" ID="6vt3fgv7ihg1b5b7vlaua3pg6n" MODIFIED="1614527250800" TEXT="(contact|kontak) (erpid|erp)"/>
+        <node CREATED="1614527250800" ID="4ilflt15tnjv0b1vcf28uf7q6k" MODIFIED="1614527250800" TEXT="(erpid|erp) (contact|kontak)"/>
       </node>
     </node>
   </node>
