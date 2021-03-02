d42aa2c25f501597d65d58fe20eaa8665b04d38e
Carik Ecosystem Builder <carik.bot@gmail.com>
2021-03-02 21:52:52 +0700

    Ecosystem Builder: community.Community

---
+++ data/community/Community/Community-recode.mm
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+  <node CREATED="1614696608535" ID="b9aa22deba98b3b20c7ac8aca2" MODIFIED="1614696608535" STYLE="bubble" TEXT="Community">
+    <hook NAME="accessories/plugins/NodeNote.properties">
+      <text>Informasi komunitas yang ada di dalam ekosistem Carik.
+</text>
+    </hook>
+    <node CREATED="1614696608535" ID="193b56735e689ae86a01d91513" MODIFIED="1614696608535" POSITION="right" TEXT="PHP Indonesia">
+      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614696608535" FOLDER="true" ID="61lrimrpaj3e6ejcmn8shl9n9u" MODIFIED="1614696608535" TEXT="*PHP Indonesia*&#10;&#10;PHP Indonesia bermaksud menghimpun, mendorong meningkatkan dan memanfaatkan potensi segenap pihak yang bergerak di bidang Pemrograman PHP dan Pemrograman lainnya yang mendukung, untuk mewujudkan suatu kondisi yang saling melengkapi dalam rangka pencapaian tujuan PHP Indonesia.&#10;&#10;PHP Indonesia bertujuan menumbuhkan, mengembangkan dan meningkatkan keahlian, produktivitas, profesionalisme, peningkatan daya saing untuk pemenuhan pasar piranti lunak nasional dalam rangka pembangunan bangsa dan negara, melalui sumber daya pemrogram berbasis PHP dalam lingkup nasional, dan meningkatkan kesejahteraan anggota.&#10;&#10;[PHP Indonesia](https://phpindonesia.id)">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node CREATED="1614696608535" ID="3sb8427stqs420ii6u3s3d091k" MODIFIED="1614696608535" TEXT="Action Type">
+        <node CREATED="1614696608535" FOLDER="true" ID="3a7vdlpi5o748vlk0f5c21a3ra" MODIFIED="1614696608535" TEXT="button"/>
+      </node>
+      <node CREATED="1614696608535" ID="2l3m8pp3lm0ga6ae639q0aphdf" MODIFIED="1614696608535" TEXT="Action">
+        <node CREATED="1614696608535" ID="3eqq333cf9adlc0cs41rc9ohp2" MODIFIED="1614696608535" TEXT="🗓 Daftar Event">
+          <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614696608535" FOLDER="true" ID="0l4pjbjh7qn8dc7rf58ah7useh" MODIFIED="1614696608535" TEXT="{ecosystem_baseurl}/services/community/php-indonesia/event-list/">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+          <node CREATED="1614696608535" ID="7nut6i8ltbrjhtchs1s8pps0gq" MODIFIED="1614696608535" TEXT="pattern">
+            <node CREATED="1614696608535" ID="06vqvaf3lvtre99735a1f3lriq" MODIFIED="1614696608535" TEXT="(jadwal|jdwl) phpid"/>
+            <node CREATED="1614696608535" ID="5ec9gni7nnl27om1uins5kkh9j" MODIFIED="1614696608535" TEXT="(jadwal|jdwl) online learning (php|phpid)"/>
+            <node CREATED="1614696608535" ID="4r7f6s5oenkgtf7agd6ncrbu99" MODIFIED="1614696608535" TEXT="(jadwal|jdwl) (meetup|streaming|online|event) (php|phpid)"/>
+            <node CREATED="1614696608535" ID="78f6r2f5j1rh53gq1t1ko7ld3b" MODIFIED="1614696608535" TEXT="online learning (phpid|php)"/>
+            <node CREATED="1614696608535" ID="1h4md09mqlofbjda2r7cm5bllc" MODIFIED="1614696608535" TEXT="(phpid|php) online learning"/>
+          </node>
+        </node>
+        <node CREATED="1614696608535" ID="2egvvm31oqlqqlgr7pvmogu5jv" MODIFIED="1614696608535" TEXT="🌟 Media Social">
+          <node BACKGROUND_COLOR="#F2F2F2" CREATED="1614696608535" FOLDER="true" ID="3166lt5gnd7ndkpp8qdpo17ivs" MODIFIED="1614696608535" TEXT="*Media PHP Indonesia*&#10;&#10;- [Situs PHP Indonesia](https://phpindonesia.id)&#10;- [Facebook Group](https://www.facebook.com/groups/phpid/)&#10;- [PHPID for Student](https://t.me/PHPIDforBusiness)&#10;- [PHPID for Business](https://t.me/PHPIDforStudent)&#10;">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+      </node>
+      <node CREATED="1614696608535" ID="7lqrbvpnptt1ivqpliipeg1mcq" MODIFIED="1614696608535" TEXT="pattern">
+        <node CREATED="1614696608535" ID="05rl5vph5r7jki0gqfgde22nu0" MODIFIED="1614696608535" TEXT="info php indonesia"/>
+        <node CREATED="1614696608535" ID="2h251mr2snlu0n5ai0v3urapo7" MODIFIED="1614696608535" TEXT="info phpid"/>
+      </node>
+    </node>
+  </node>
+</map>
