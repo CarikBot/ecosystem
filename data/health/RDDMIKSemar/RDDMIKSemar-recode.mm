045ec429a397682dbb5a4b34e0cc636cffc83f91
Carik Ecosystem Builder <carik.bot@gmail.com>
2021-03-14 14:49:42 +0700

    Ecosystem Builder: health.PMIJateng,health.RDDMIKSemar

--- data/health/RDDMIKSemar/RDDMIKSemar-recode.mm
+++ data/health/RDDMIKSemar/RDDMIKSemar-recode.mm
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <map version="0.8.1">
-  <node CREATED="1615707713129" ID="62ura2p7lvlji4gkg1f40ah8k1" MODIFIED="1615707713129" STYLE="bubble" TEXT="RDDMIKSemar">
+  <node CREATED="1615708107256" ID="62ura2p7lvlji4gkg1f40ah8k1" MODIFIED="1615708107257" STYLE="bubble" TEXT="RDDMIKSemar">
     <hook NAME="accessories/plugins/NodeNote.properties">
       <text>*RDD MIK Semar*
 
@@ -16,66 +16,66 @@ misal:
 Informasi ini dipersembahkan oleh *Relawan Donor Darah MIK Semar*
 </text>
     </hook>
-    <node CREATED="1615707713129" ID="72vmoiu1ur5ljoukkduvaod6nm" MODIFIED="1615707713129" POSITION="right" TEXT="❣️ Syarat">
-      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1615707713129" ID="2cqm1gi8q1hla0drontlvv0tdf" MODIFIED="1615707713129" TEXT="*Syarat Donor Darah*&#10;&#10;- Usia 17-60 tahun (usia 17 tahun diperbolehkan menjadi donor bila mendapat izin tertulis dari orangtua)&#10;- Berat badan minimal 45 kg&#10;- Temperatur tubuh 36,6 – 37,5 derajat Celcius&#10;- Tekanan darah baik yaitu sistole = 110-160 mmHg, diastole = 70-100 mmHg&#10;- Denyut nadi teratur yaitu sekitar 50-100 kali/menit&#10;- Hemoglobin perempuan minimal 12 gram, sedangkan untuk laki-laki minimal 12,5 gram&#10;- Jumlah penyumbangan per tahun paling banyak 5 kali dengan jarak penyumbangan sekurang-kurangnya 3 bulan&#10;- Calon donor dapat mengambil dan menandatangani formulir pendaftaran, lalu menjalani pemeriksaan pendahuluan, seperti kondisi berat badan, HB, golongan darah, dan dilanjutkan dengan pemeriksaan dokter &#10;&#10;Sumber: [PMI](http://www.pmi.or.id/)">
+    <node CREATED="1615708107257" ID="72vmoiu1ur5ljoukkduvaod6nm" MODIFIED="1615708107257" POSITION="right" TEXT="❣️ Syarat">
+      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1615708107257" ID="2cqm1gi8q1hla0drontlvv0tdf" MODIFIED="1615708107257" TEXT="*Syarat Donor Darah*&#10;&#10;- Usia 17-60 tahun (usia 17 tahun diperbolehkan menjadi donor bila mendapat izin tertulis dari orangtua)&#10;- Berat badan minimal 45 kg&#10;- Temperatur tubuh 36,6 – 37,5 derajat Celcius&#10;- Tekanan darah baik yaitu sistole = 110-160 mmHg, diastole = 70-100 mmHg&#10;- Denyut nadi teratur yaitu sekitar 50-100 kali/menit&#10;- Hemoglobin perempuan minimal 12 gram, sedangkan untuk laki-laki minimal 12,5 gram&#10;- Jumlah penyumbangan per tahun paling banyak 5 kali dengan jarak penyumbangan sekurang-kurangnya 3 bulan&#10;- Calon donor dapat mengambil dan menandatangani formulir pendaftaran, lalu menjalani pemeriksaan pendahuluan, seperti kondisi berat badan, HB, golongan darah, dan dilanjutkan dengan pemeriksaan dokter &#10;&#10;Sumber: [PMI](http://www.pmi.or.id/)">
         <font NAME="SansSerif" SIZE="10"/>
       </node>
-      <node CREATED="1615707713129" ID="5mdlttp3ktr19gb9b0cdkvajs4" MODIFIED="1615707713129" TEXT="pattern">
-        <node CREATED="1615707713129" ID="44u44ahocihlkg4en6hm6r4ses" MODIFIED="1615707713129" TEXT="(syarat|ketentuan) donor"/>
-        <node CREATED="1615707713129" ID="7vkiodcsing44i35cv6gdt8os9" MODIFIED="1615707713129" TEXT="blood (donation|donor|donors) (requirement|requirements)"/>
+      <node CREATED="1615708107257" ID="5mdlttp3ktr19gb9b0cdkvajs4" MODIFIED="1615708107257" TEXT="pattern">
+        <node CREATED="1615708107257" ID="44u44ahocihlkg4en6hm6r4ses" MODIFIED="1615708107257" TEXT="(syarat|ketentuan) donor"/>
+        <node CREATED="1615708107257" ID="7vkiodcsing44i35cv6gdt8os9" MODIFIED="1615708107257" TEXT="blood (donation|donor|donors) (requirement|requirements)"/>
       </node>
     </node>
-    <node CREATED="1615707713129" ID="3gql8802j8uobnqhh6lg5pej5k" MODIFIED="1615707713129" POSITION="right" TEXT="⭐️ Cara">
-      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1615707713129" ID="6cbhrjbdunml1b5e0g521mbhq4" MODIFIED="1615707713129" TEXT="*Cara Berdonor Darah*&#10;&#10;Caranya mudah. Tinggal datang ke UDD terdekat dan lakukan langkah-langkah berikut:&#10;&#10;- Mengambil formulir pendaftaran donor&#10;- Membaca pesan donor dan mengisi formulir donor yang berisi tentang pertanyaan riwayat medis (inform consent) dan identitas donor&#10;- Menandatangani formulir pendaftaran&#10;- Pemeriksaan pendahuluan yaitu penimbangan berat badan, pemeriksaan Hb, dan golongan darah&#10;- Pemeriksaan dokter termasuk wawancara dan pemeriksaan tekanan darah&#10;- Pengambilan darah dan sample darah&#10;- Pengambilan kartu donor&#10;- Istirahat (donor disediakan makanan pengganti)&#10;&#10;Sumber: [PMI](http://www.pmi.or.id/)">
+    <node CREATED="1615708107257" ID="3gql8802j8uobnqhh6lg5pej5k" MODIFIED="1615708107257" POSITION="right" TEXT="⭐️ Cara">
+      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1615708107257" ID="6cbhrjbdunml1b5e0g521mbhq4" MODIFIED="1615708107257" TEXT="*Cara Berdonor Darah*&#10;&#10;Caranya mudah. Tinggal datang ke UDD terdekat dan lakukan langkah-langkah berikut:&#10;&#10;- Mengambil formulir pendaftaran donor&#10;- Membaca pesan donor dan mengisi formulir donor yang berisi tentang pertanyaan riwayat medis (inform consent) dan identitas donor&#10;- Menandatangani formulir pendaftaran&#10;- Pemeriksaan pendahuluan yaitu penimbangan berat badan, pemeriksaan Hb, dan golongan darah&#10;- Pemeriksaan dokter termasuk wawancara dan pemeriksaan tekanan darah&#10;- Pengambilan darah dan sample darah&#10;- Pengambilan kartu donor&#10;- Istirahat (donor disediakan makanan pengganti)&#10;&#10;Sumber: [PMI](http://www.pmi.or.id/)">
         <font NAME="SansSerif" SIZE="10"/>
       </node>
-      <node CREATED="1615707713129" ID="04ev85hbhp66sf5l0memt7cmse" MODIFIED="1615707713129" TEXT="pattern">
-        <node CREATED="1615707713129" ID="3mnh78tjevfa1od57sol7becsd" MODIFIED="1615707713129" TEXT="cara donor darah"/>
-        <node CREATED="1615707713129" ID="02ia8uhc8tnvjont4kvqdsdgjm" MODIFIED="1615707713129" TEXT="blood (donation|donor|donors) way"/>
+      <node CREATED="1615708107257" ID="04ev85hbhp66sf5l0memt7cmse" MODIFIED="1615708107257" TEXT="pattern">
+        <node CREATED="1615708107257" ID="3mnh78tjevfa1od57sol7becsd" MODIFIED="1615708107257" TEXT="cara donor darah"/>
+        <node CREATED="1615708107257" ID="02ia8uhc8tnvjont4kvqdsdgjm" MODIFIED="1615708107257" TEXT="blood (donation|donor|donors) way"/>
       </node>
     </node>
-    <node CREATED="1615707713129" ID="7h8l02b0plihd7pjm99i6jr2ig" MODIFIED="1615707713129" POSITION="right" TEXT="🥇 Manfaat">
-      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1615707713129" ID="28d0iaqle8rk1mr9vjprpl5aa6" MODIFIED="1615707713129" TEXT="*Manfaat Berdonor Darah*&#10;&#10;Orang yang rutin donor darah punya resiko lebih rendah terkena serangan jantung loh. Mau tau ga sih sebab nya apa? Yuk disimak ...&#10;&#10;Hal tersebut disebabkan karena transfusi darah atau pengambilan darah mampu menurunkan kadar besi di dalam tubuh. Kadar besi ini mempengaruhi seberapa kental tekstur darah. Setelah pengambilan darah atau transfusi maka otomatis akan merangsang tubuh kita untuk membentuk sel darah baru. Tubuh akan melakukan proses alami untuk mengembalikan kadar zat besi. &#10;Selain itu, orang yang rutin melakukan donor darah juga akan rutin mengecek kesehatan nya. Itulah mengapa orang yang rutin melakukan donor darah akan mempunyai resiko lebih rendah terkena serangan jantung. Ayo kita donor darah. Karena selain membantu orang lain, donor darah juga menyehatkan diri kita sendiri loh&#10;&#10;Ini manfaatnya lainnya bagi Anda jika melakukan donor darah:&#10;&#10;- Menjaga kesehatan jantung&#10;- Meningkatkan produksi sel darah merah&#10;- Membantu menurunkan berat badan&#10;- Mendapatkan kesehatan psikologis&#10;- Mendeteksi penyakit serius &#10;&#10;Sumber: [PMI](http://www.pmi.or.id/index.php/aktivitas/pelayanan/donor-darah/donor-sekarang.html?showall=&amp;start=3)">
+    <node CREATED="1615708107257" ID="7h8l02b0plihd7pjm99i6jr2ig" MODIFIED="1615708107257" POSITION="right" TEXT="🥇 Manfaat">
+      <node BACKGROUND_COLOR="#F2F2F2" CREATED="1615708107257" ID="28d0iaqle8rk1mr9vjprpl5aa6" MODIFIED="1615708107257" TEXT="*Manfaat Berdonor Darah*&#10;&#10;Orang yang rutin donor darah punya resiko lebih rendah terkena serangan jantung loh. Mau tau ga sih sebab nya apa? Yuk disimak ...&#10;&#10;Hal tersebut disebabkan karena transfusi darah atau pengambilan darah mampu menurunkan kadar besi di dalam tubuh. Kadar besi ini mempengaruhi seberapa kental tekstur darah. Setelah pengambilan darah atau transfusi maka otomatis akan merangsang tubuh kita untuk membentuk sel darah baru. Tubuh akan melakukan proses alami untuk mengembalikan kadar zat besi. &#10;Selain itu, orang yang rutin melakukan donor darah juga akan rutin mengecek kesehatan nya. Itulah mengapa orang yang rutin melakukan donor darah akan mempunyai resiko lebih rendah terkena serangan jantung. Ayo kita donor darah. Karena selain membantu orang lain, donor darah juga menyehatkan diri kita sendiri loh&#10;&#10;Ini manfaatnya lainnya bagi Anda jika melakukan donor darah:&#10;&#10;- Menjaga kesehatan jantung&#10;- Meningkatkan produksi sel darah merah&#10;- Membantu menurunkan berat badan&#10;- Mendapatkan kesehatan psikologis&#10;- Mendeteksi penyakit serius &#10;&#10;Sumber: [PMI](http://www.pmi.or.id/index.php/aktivitas/pelayanan/donor-darah/donor-sekarang.html?showall=&amp;start=3)">
         <font NAME="SansSerif" SIZE="10"/>
       </node>
-      <node CREATED="1615707713129" ID="3ht0jfh2hhm8edg9gob4d32e9k" MODIFIED="1615707713129" TEXT="pattern">
-        <node CREATED="1615707713129" ID="5hkrbstjk4knmabqbarb9a1aaq" MODIFIED="1615707713129" TEXT="(manfaat|kegunaan|guna) donor darah"/>
-        <node CREATED="1615707713130" ID="28tfvuio75gp4hgjdfc7q55j0a" MODIFIED="1615707713130" TEXT="(benefit|benefits) of blood (donation|donor|donors)"/>
-        <node CREATED="1615707713130" ID="2ti68lu7cks4k0dce4pi04786j" MODIFIED="1615707713130" TEXT="(benefit|benefits) blood (donation|donor|donors)"/>
+      <node CREATED="1615708107257" ID="3ht0jfh2hhm8edg9gob4d32e9k" MODIFIED="1615708107257" TEXT="pattern">
+        <node CREATED="1615708107257" ID="5hkrbstjk4knmabqbarb9a1aaq" MODIFIED="1615708107257" TEXT="(manfaat|kegunaan|guna) donor darah"/>
+        <node CREATED="1615708107257" ID="28tfvuio75gp4hgjdfc7q55j0a" MODIFIED="1615708107257" TEXT="(benefit|benefits) of blood (donation|donor|donors)"/>
+        <node CREATED="1615708107257" ID="2ti68lu7cks4k0dce4pi04786j" MODIFIED="1615708107257" TEXT="(benefit|benefits) blood (donation|donor|donors)"/>
       </node>
     </node>
-    <node CREATED="1615707713130" ID="6i770tma53uu1eq2ku9fed3e3f" MODIFIED="1615707713130" POSITION="right" TEXT="📋 Pengajuan">
-      <node BACKGROUND_COLOR="#EEEEEE" CREATED="1615707713130" ID="082vn2uf53ecitvomldf36miqs" MODIFIED="1615707713130" TEXT="*Pengajuan Donor*&#10;&#10;Membuat surat permohonan yang ditujukan ke PMI kota atau kabupaten sesuai lokasi donor. Nanti akan dicek dengan jadwal donor darah yang sudah masuk.&#10;&#10;Khusus untuk wilayah Semarang, *Relawan Donor Darah MIK Semar* menyediakan layanan [Permintaan Bantuan Pencarian Donor Darah Pengganti](http://bit.ly/rddcaridarah)">
+    <node CREATED="1615708107257" ID="6i770tma53uu1eq2ku9fed3e3f" MODIFIED="1615708107257" POSITION="right" TEXT="📋 Pengajuan">
+      <node BACKGROUND_COLOR="#EEEEEE" CREATED="1615708107257" ID="082vn2uf53ecitvomldf36miqs" MODIFIED="1615708107257" TEXT="*Pengajuan Donor*&#10;&#10;Membuat surat permohonan yang ditujukan ke PMI kota atau kabupaten sesuai lokasi donor. Nanti akan dicek dengan jadwal donor darah yang sudah masuk.&#10;&#10;Khusus untuk wilayah Semarang, *Relawan Donor Darah MIK Semar* menyediakan layanan [Permintaan Bantuan Pencarian Donor Darah Pengganti](http://bit.ly/rddcaridarah)">
         <font NAME="SansSerif" SIZE="10"/>
       </node>
-      <node CREATED="1615707713130" ID="60go8khu5nj3cq3n8otbmb70jb" MODIFIED="1615707713130" TEXT="pattern">
-        <node CREATED="1615707713130" ID="5qrbfudprbl5rok8j4efcddced" MODIFIED="1615707713130" TEXT="pengajuan donor"/>
-        <node CREATED="1615707713130" ID="2ave3kp6uc6dacb7g1rfb8o13a" MODIFIED="1615707713130" TEXT="pengajuan acara donor"/>
+      <node CREATED="1615708107257" ID="60go8khu5nj3cq3n8otbmb70jb" MODIFIED="1615708107257" TEXT="pattern">
+        <node CREATED="1615708107257" ID="5qrbfudprbl5rok8j4efcddced" MODIFIED="1615708107257" TEXT="pengajuan donor"/>
+        <node CREATED="1615708107257" ID="2ave3kp6uc6dacb7g1rfb8o13a" MODIFIED="1615708107257" TEXT="pengajuan acara donor"/>
       </node>
     </node>
-    <node CREATED="1615707713130" ID="2394cm1oo6b5d7tbaeok4ied7l" MODIFIED="1615707713130" POSITION="left" TEXT="⏱ Periode">
-      <node BACKGROUND_COLOR="#EEEEEE" CREATED="1615707713130" ID="6ru1k49nql3lpuahdrheavgkfi" MODIFIED="1615707713130" TEXT="*Periode Berdonor Darah*&#10;&#10;Karena kebutuhannya berbeda, maka jumlah maksimal donor yang dianjurkan untuk pria dan wanita juga berbeda. Untuk pria, boleh donor darah setiap 12 minggu atau tiga bulan sekali. Sementara untuk wanita, waktu donor darah dalam setahun adalah setiap 16 minggu atau empat bulan sekali.">
+    <node CREATED="1615708107257" ID="2394cm1oo6b5d7tbaeok4ied7l" MODIFIED="1615708107257" POSITION="left" TEXT="⏱ Periode">
+      <node BACKGROUND_COLOR="#EEEEEE" CREATED="1615708107257" ID="6ru1k49nql3lpuahdrheavgkfi" MODIFIED="1615708107257" TEXT="*Periode Berdonor Darah*&#10;&#10;Karena kebutuhannya berbeda, maka jumlah maksimal donor yang dianjurkan untuk pria dan wanita juga berbeda. Untuk pria, boleh donor darah setiap 12 minggu atau tiga bulan sekali. Sementara untuk wanita, waktu donor darah dalam setahun adalah setiap 16 minggu atau empat bulan sekali.">
         <font NAME="SansSerif" SIZE="10"/>
       </node>
-      <node CREATED="1615707713130" ID="23b1uh1ou8se8tp6tifb4pelrc" MODIFIED="1615707713130" TEXT="pattern">
-        <node CREATED="1615707713130" ID="0i2fu34v2gllm4ru8uhqa7vke8" MODIFIED="1615707713130" TEXT="periode donor darah"/>
-        <node CREATED="1615707713130" ID="7pr3msci9err8pv7alsubqnb67" MODIFIED="1615707713130" TEXT="blood (donation|donor|donors) periode"/>
-        <node CREATED="1615707713130" ID="4oj246434nr65v4l2dispt26qj" MODIFIED="1615707713130" TEXT="@Tanya:berapa bulan sekali donor darah"/>
+      <node CREATED="1615708107257" ID="23b1uh1ou8se8tp6tifb4pelrc" MODIFIED="1615708107257" TEXT="pattern">
+        <node CREATED="1615708107257" ID="0i2fu34v2gllm4ru8uhqa7vke8" MODIFIED="1615708107257" TEXT="periode donor darah"/>
+        <node CREATED="1615708107257" ID="7pr3msci9err8pv7alsubqnb67" MODIFIED="1615708107257" TEXT="blood (donation|donor|donors) periode"/>
+        <node CREATED="1615708107257" ID="4oj246434nr65v4l2dispt26qj" MODIFIED="1615708107257" TEXT="@Tanya:berapa bulan sekali donor darah"/>
       </node>
     </node>
-    <node CREATED="1615707713130" ID="6l5uhc0tjpqpa27b4sd7mgc3nd" MODIFIED="1615707713130" POSITION="left" TEXT="💉 Plasma">
-      <node BACKGROUND_COLOR="#EEEEEE" CREATED="1615707713130" ID="4blhnrv9ljqk464nv861c5scql" MODIFIED="1615707713130" TEXT="*Donor Plasma Konvalesen*&#10;&#10;Donor plasma konvalesen adalah metode pengambilan darah plasma dari penyintas COVID-19 yang dapat diberikan sebagai terapi untuk pasien COVID-19 yang sedang dirawat.&#10;&#10;Apa saja syarat menjadi donor plasma konvalesen :&#10;- Sudah sembuh dari COVID-19,&#10;- Sehat&#10;- Bebas gejala selama 14 hari setelah sembuh&#10;- Memperlihatkan hasil RT PCR negative 1 kali&#10;- Berat badan 55 kg&#10;- Berusia 18-60 tahun&#10;- Disarankan laki-laki&#10;&#10;*Bagaimana cara menjadi donor plasma?*&#10;&#10;Para penyintas yang telah memenuhi kriteria dapat menghubungi UDD PMI. Petugas akan mengatur waktu untuk pemeriksaan dan pengambilan sampel darah, jika memenuhi syarat, pengambilan donor plasma konvalesen akan dilakukan menggunakan metode apheresis.&#10;&#10;Informai selengkapnya bisa dibaca melalui situs [covid19](https://covid19.go.id/p/berita/para-penyintas-covid-19-ayo-donor-plasma-konvalesen)&#10;">
+    <node CREATED="1615708107257" ID="6l5uhc0tjpqpa27b4sd7mgc3nd" MODIFIED="1615708107257" POSITION="left" TEXT="💉 Plasma">
+      <node BACKGROUND_COLOR="#EEEEEE" CREATED="1615708107257" ID="4blhnrv9ljqk464nv861c5scql" MODIFIED="1615708107257" TEXT="*Donor Plasma Konvalesen*&#10;&#10;Donor plasma konvalesen adalah metode pengambilan darah plasma dari penyintas COVID-19 yang dapat diberikan sebagai terapi untuk pasien COVID-19 yang sedang dirawat.&#10;&#10;Apa saja syarat menjadi donor plasma konvalesen :&#10;- Sudah sembuh dari COVID-19,&#10;- Sehat&#10;- Bebas gejala selama 14 hari setelah sembuh&#10;- Memperlihatkan hasil RT PCR negative 1 kali&#10;- Berat badan 55 kg&#10;- Berusia 18-60 tahun&#10;- Disarankan laki-laki&#10;&#10;*Bagaimana cara menjadi donor plasma?*&#10;&#10;Para penyintas yang telah memenuhi kriteria dapat menghubungi UDD PMI. Petugas akan mengatur waktu untuk pemeriksaan dan pengambilan sampel darah, jika memenuhi syarat, pengambilan donor plasma konvalesen akan dilakukan menggunakan metode apheresis.&#10;&#10;Informai selengkapnya bisa dibaca melalui situs [covid19](https://covid19.go.id/p/berita/para-penyintas-covid-19-ayo-donor-plasma-konvalesen)&#10;">
         <font NAME="SansSerif" SIZE="10"/>
       </node>
-      <node CREATED="1615707713130" ID="52930uo2dq81l3l7pd59s5au6l" MODIFIED="1615707713130" TEXT="Action Type">
-        <node CREATED="1615707713130" ID="6mn0iu7u8gqa07enphv0vntq76" MODIFIED="1615707713130" TEXT="button"/>
+      <node CREATED="1615708107257" ID="52930uo2dq81l3l7pd59s5au6l" MODIFIED="1615708107257" TEXT="Action Type">
+        <node CREATED="1615708107257" ID="6mn0iu7u8gqa07enphv0vntq76" MODIFIED="1615708107257" TEXT="button"/>
       </node>
-      <node CREATED="1615707713130" ID="7mtqv9bm2soobsrvk0ok9jr7ri" MODIFIED="1615707713130" TEXT="Action">
-        <node CREATED="1615707713130" ID="2eodnkrfjfa9lah11jk635q5ii" MODIFIED="1615707713130" TEXT="🏠 Kembali"/>
+      <node CREATED="1615708107257" ID="7mtqv9bm2soobsrvk0ok9jr7ri" MODIFIED="1615708107257" TEXT="Action">
+        <node CREATED="1615708107257" ID="2eodnkrfjfa9lah11jk635q5ii" MODIFIED="1615708107257" TEXT="🏠 Kembali"/>
       </node>
-      <node CREATED="1615707713130" ID="2rbgvltgo6lpoo9uqob4rdcoh0" MODIFIED="1615707713130" TEXT="pattern">
-        <node CREATED="1615707713130" ID="1s0h36606pqul7jcgl026gtauj" MODIFIED="1615707713130" TEXT="donor plasma"/>
-        <node CREATED="1615707713130" ID="45g3b7alhq1u4in1pbriib6r8t" MODIFIED="1615707713130" TEXT="(konvalesen|konvaselen)"/>
+      <node CREATED="1615708107257" ID="2rbgvltgo6lpoo9uqob4rdcoh0" MODIFIED="1615708107257" TEXT="pattern">
+        <node CREATED="1615708107257" ID="1s0h36606pqul7jcgl026gtauj" MODIFIED="1615708107257" TEXT="donor plasma"/>
+        <node CREATED="1615708107257" ID="45g3b7alhq1u4in1pbriib6r8t" MODIFIED="1615708107257" TEXT="(konvalesen|konvaselen)"/>
       </node>
     </node>
   </node>
