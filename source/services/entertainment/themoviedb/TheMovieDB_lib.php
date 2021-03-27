7ab28058b4525994922c69df15352a4d747c257f
Luri <luri@kioss.com>
2021-03-27 16:49:04 +0700

    Add Entertainment package

---
+++ source/services/entertainment/themoviedb/TheMovieDB_lib.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * USAGE
+ *   $TheMovieDB = new TheMovieDB;
+ *   $TheMovieDB->Key = '';
+ *   
+ *   $data = $TheMovieDB->SearchMovie('league');
+ * 
+ * @date       20-02-2020 21:28
+ * @category   AksiIDE
+ * @package    TheMovieDB
+ * @subpackage
+ * @copyright  Copyright (c) 2013-endless AksiIDE
+ * @license
+ * @version
+ * @link       http://www.aksiide.com
+ * @since
+ */
+
+//namespace Carik\TheMovieDB;
+
+/**
+ * TheMovieDB Handler
+ *
+ * @author Luri Darmawan <luri@carik.id>
+ */
+class TheMovieDB {
+  const BASE_URL = 'https://api.themoviedb.org/3/';
+  public $Key = '';
+
+  public function __construct(){
+    $this->Referer = @$_SERVER['HTTP_REFERER'];
+  }
+
+  public function Search($AKeyword){
+    if (empty($this->Key)) return false;
+    $url = self::BASE_URL . "search/movie?api_key=".$this->Key."&language=en-US&page=1&include_adult=false&query=".urlencode($AKeyword);
+    $data = @file_get_contents($url);
+    if (empty($data)) return [];
+    $dataAsArray = json_decode($data, true);
+    if (0==$dataAsArray['total_results']) return [];
+
+    $movieList = $dataAsArray['results'];
+    return $movieList;
+  }
+
+  public function Detail($AID){
+    if (empty($this->Key)) return false;
+    $url = self::BASE_URL . "movie/$AID?api_key=".$this->Key;
+    $data = @file_get_contents($url);
+    if (empty($data)) return false;
+    $dataAsArray = json_decode($data, true);
+    return $dataAsArray;
+  }
+
+}
---
+++ source/services/entertainment/themoviedb/detail.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * USAGE
+ *   curl "http://localhost/services/ecosystem/entertainment/themoviedb/detail/" -d "id=434769"
+ *   curl "http://ecosystem.carik.id/services/entertainment/themoviedb/detail/" -d "id=434769"
+ * 
+ * @date       04-01-2021 01:35
+ * @category   Education
+ * @package    Civitas Directory Library
+ * @subpackage
+ * @copyright  Copyright (c) 2013-endless AksiIDE
+ * @license
+ * @version
+ * @link       http://www.aksiide.com
+ * @since
+ */
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+//error_reporting(E_NONE);
+include_once "../../config.php";
+require_once "../../lib/lib.php";
+require_once "./TheMovieDB_lib.php";
+const BASE_IMAGE = 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/';
+
+$Key = $Config['packages']['entertainment']['themoviedb']['key'];
+if (empty($Key)) Output(200, 'Maaf, belum bisa akses ke pusat info perfilman.');
+
+$ID = urldecode(@$_GET['id']);
+if (empty($ID)) $ID = urldecode(@$_POST['id']);
+if (empty($ID)) Output(0, 'Maaf, pencarian info film tidak lengkap.');
+
+$Text = "*Info Film*\n";
+
+$TheMovieDB = new TheMovieDB;
+$TheMovieDB->Key = $Key;
+$movie = $TheMovieDB->Detail($ID);
+
+if (!$movie) Output(0, "Maaf, informasi lengkap tentang film tersebut tidak kami temukan.");
+
+$IMDBid = $movie['imdb_id'];
+$posterPath = @$movie['poster_path'];
+$image = BASE_IMAGE . $posterPath;
+$Text .= "\n*$movie[title]*";
+
+$Text .= "\nGenre: ";
+foreach ($movie['genres'] as $genre) {
+  $Text .= $genre['name']. ',';
+}
+$Text = rtrim($Text, ',');
+$Text .= "\nRelease: ".substr($movie['release_date'], 0, 4);
+$Text .= "\n_$movie[overview]_";
+$Text .= "\n[Poster]($image)";
+
+die($Text);
+Output( 0, $Text);
