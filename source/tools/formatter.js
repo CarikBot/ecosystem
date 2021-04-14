ba39d0d52832c4e0cc0328574253e8d6eb04d263
Luri <luri@kioss.com>
2021-04-14 12:53:03 +0700

    formatter: remove tags

--- source/tools/formatter.js
+++ source/tools/formatter.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { exit } = require('process');
 var xmlFormatter = require('xml-formatter');
 
 const main = async () => {
@@ -8,6 +9,10 @@ const main = async () => {
     if (fileName == undefined) return false;
     const dataAsString = await fs.readFileSync(path.resolve(fileName), { encoding: 'utf-8' });
     var formattedXml = xmlFormatter(dataAsString);
+
+    // remove tag
+    formattedXml = formattedXml.replace(/ CREATED="[a-zA-Z0-9_]+"/g, '');
+    formattedXml = formattedXml.replace(/ MODIFIED="[a-zA-Z0-9_]+"/g, '');
 
     // write to file
     fs.writeFile(path.resolve(fileName), formattedXml, function (err){
