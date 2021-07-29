e20a505539703d5cea1ed2342088b8b2c4e5c72c
Luri <luri@kioss.com>
2021-07-29 22:50:03 +0700

    health: WisataBugar

---
+++ data/health/WisataBugar/README.md
@@ -0,0 +1,8 @@
+# Wisata Bugar
+`category: health`
+
+Wisata Bugar merupakan komunitas Sehat Tanpa Obat menggunakan basis olah napas dengan metode Napas Ritme.
+
+Official website: [wisatabugarofficial.com](https://www.wisatabugarofficial.com/)
+
+![wisata bugar](WisataBugar.png)
---
+++ data/health/WisataBugar/WisataBugar.mm
@@ -0,0 +1,128 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+    <node ID="62ura2p7lvlji4gkg1f40ah8k1" STYLE="bubble" TEXT="WisataBugar">
+        <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                Wisata Bugar merupakan komunitas Sehat Tanpa Obat menggunakan basis olah napas dengan metode Napas Ritme.
+Informasi lebih lengkap dapat dilihat pada website: [WisataBugarOfficial.com](https://www.WisataBugarOfficial.com).
+
+Free sesi latihan dapat dilihat pada channel [MasGunggung](https://bit.ly/MasGunggung).
+            </text>
+        </hook>
+        <node ID="72vmoiu1ur5ljoukkduvaod6nm" POSITION="right" TEXT="❣️ Olah nafas">
+            <node BACKGROUND_COLOR="#F2F2F2" ID="2cqm1gi8q1hla0drontlvv0tdf" TEXT="Olah napas ini telah disesuaikan untuk masing-masing fungsi dan kegunaannya">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node ID="6te7miqms4m14hh3u7ll13oaua" TEXT="Action Type">
+                <node ID="6dfgud5iku69c2103ec63g2eje" TEXT="menu"/>
+            </node>
+            <node ID="6empsn280c128dfsl3689cottm" TEXT="Action">
+                <node FOLDER="true" ID="239908f9a39heb6hpvj8tcsct6" TEXT="Pasien covid gejala ringan">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="1qp0mc1dokh4vag9bat0ibeg60" TEXT="*Pasien covid gejala ringan*&#10;&#10;1. Selalu gunakan napas Hidung-Hidung DAN Diafragma&#10;2. Ritme 5:5 (15 menit) untuk menaikkan saturasi: https://www.youtube.com/watch?v=cYD8g7dBe3k&#10;3. Ritme 2:4 (15 menit) untuk menstimulasi syaraf Vagus: https://www.youtube.com/watch?v=yO5PvNP-3kw&#10;4. Ritme 3:3 (6 menit) untuk menstimulasi syaraf Parasimpatik: https://www.youtube.com/watch?v=jpV3RdNEL-g">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="115ojqneth0b6fm18gfj1jtf3m" TEXT="pattern">
+                        <node ID="1tioufg2bdpug40fsho30g27gr" TEXT="napas untuk pasien covid gejala ringan"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="2gp7r832a0fkuc12h848qoq0id" TEXT="Pasien covid tanpa gejala atau gejala ringan">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="39fb0kdkgi9ubek32qnnabgq6p" TEXT="*Pasien covid tanpa gejala atau gejala ringan*&#10;&#10;1. Selalu gunakan napas Hidung-Hidung DAN Diafragma&#10;2. Ritme 5:5 (15 menit) untuk menaikkan saturasi: https://www.youtube.com/watch?v=cYD8g7dBe3k&#10;3. Ritme 3:3 + 2:4 + IH 90 detik untuk menaikkan stimulasi Parasimpatik: https://www.youtube.com/watch?v=pZUQmq9WD_Q&#10;">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="3pm7s8tijhjt3gns6v00sniasi" TEXT="pattern">
+                        <node ID="10idgrv9o9cn5roikq51gd80an" TEXT="napas untuk pasien covid tanpa gejala"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="683rk1om6cahp3roviqsinpqdr" TEXT="Pasien covid gejala sedang (SpO2 turun)">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="3v0u1aj68o3364ta8i51svc87l" TEXT="*Pasien covid gejala sedang (SpO2 turun)*&#10;&#10;1. Selalu gunakan napas Hidung-Hidung DAN Diafragma&#10;2. Ritme 5:5 (10 menit) terlebih dahulu: https://www.youtube.com/watch?v=cYD8g7dBe3k&#10;3. Jika tidak mampu, lakukan Ritme 3:3 selama 6 menit: https://www.youtube.com/watch?v=jpV3RdNEL-g&#10;4. Jika tidak mampu, lakukan Ritme 2:2 selama 6 menit: https://www.youtube.com/watch?v=4ivrlvpRxS0&#10;5. Bertahap-tahap mulai ditingkatkan dari Ritme paling rendah yang mampu dilakukan hingga mencapai standar di Ritme 5:5&#10;">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="1fv1hrfi2704hi72ktq02lt7ak" TEXT="pattern">
+                        <node ID="79vn4oauhmr9lqqesvp3j3igeb" TEXT="napas untuk pasien covid gejala sedang"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="3odh9v9124tpt58bp8oaboub7q" TEXT="Komorbid Asma">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="1k40uu10cckgjm89o66a7lepcs" TEXT="*Komorbid Asma*&#10;&#10;1. Selalu gunakan napas Hidung-Hidung DAN Diafragma&#10;2. [Ritme 2:4](https://www.youtube.com/watch?v=yO5PvNP-3kw) (15 menit)&#10;3. [Ritme 4:8](https://www.youtube.com/watch?v=MnuVTgJCRAU) (15 menit)&#10;">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="32p45a86f6ou11vfsi9aabhhad" TEXT="pattern">
+                        <node ID="0340bni137vgtft77si4avpbu7" TEXT="komorbid asma"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="15cr7ie8p7eghtgom5hq6uvts2" TEXT="Perbaikan Inflamasi">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="0hphtfpvt75t14ropjs6im89i9" TEXT="*Perbaikan Inflamasi*&#10;&#10;Gunakan Stimulasi frekwensi 0.1 Hz + 0.17 Hz + 0.02 Hz + 0.1 Hz : https://www.youtube.com/watch?v=pZUQmq9WD_Q&#10;&#10;*Latihan*&#10;1. [Hari ke-1](https://www.youtube.com/watch?v=MepS6YraX84)&#10;2. [Hari ke-2](https://www.youtube.com/watch?v=cJofYVCtaL4)&#10;3. [Hari ke-3](https://www.youtube.com/watch?v=RNMvhM9Vnps)&#10;4. [Hari ke-4](https://www.youtube.com/watch?v=Z3Dpne5H_yQ)&#10;5. [Hari ke-5](https://www.youtube.com/watch?v=baHQD1oalEc)&#10;&#10;">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="6bg6uhceg86ngk0delpfra9ror" TEXT="pattern">
+                        <node ID="54h18hcf3ar8a5gdpp4sp0i33v" TEXT="perbaikan inflamasi"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="6qnnalcirdg1982s6893vgv5om" TEXT="Slow Breathing dan Deep Breathing">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="7pat62piaaql66hqn5e6h45h6d" TEXT="*Slow Breathing dan Deep Breathing*&#10;&#10;1. Ritme 5:5 (15 menit) : https://www.youtube.com/watch?v=cYD8g7dBe3k&#10;2. Ritme 10:10 (15 menit) : https://www.youtube.com/watch?v=bASqSggJjTc&#10;3. Ritme 15:15 (15 menit) : https://www.youtube.com/watch?v=PkvssmLByco&#10;">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="2cmpllal3snpvsktbcksmlci00" TEXT="pattern">
+                        <node ID="58sebc7gsd3evh47u6uha1ir6o" TEXT="slow breathing"/>
+                        <node ID="3hdkrov3i0ah78mt0n0p2rjjeu" TEXT="deep breathing"/>
+                    </node>
+                </node>
+            </node>
+            <node FOLDER="true" ID="5mdlttp3ktr19gb9b0cdkvajs4" TEXT="pattern">
+                <node ID="44u44ahocihlkg4en6hm6r4ses" TEXT="info olah (nafas|napas)"/>
+            </node>
+        </node>
+        <node ID="3uc571831vlmcoav8c0cau5jia" POSITION="right" TEXT="&#128214; Pengetahuan">
+            <node BACKGROUND_COLOR="#F2F2F2" ID="3f57a3j8rdgq5vcc00jeg4icb0" TEXT="*Pengetahuan*&#10;&#10;Beberapa informasi ini akan banyak berguna untuk pemahaman tentang Napas Ritme ini. ">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node ID="21a58voefki1ms5vmd6fmrf0ds" TEXT="Action Type">
+                <node ID="2ac8lu3dtgu8jtaiumgl9bqqkv" TEXT="menu"/>
+            </node>
+            <node ID="1hp5m8703f0ounsr53paon1rmu" TEXT="Action">
+                <node FOLDER="true" ID="3jmblm4i079jqrg7l4hjk2j8nm" TEXT="Oxymeter dan Fight Happy Hypoxia">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="77krshfgl805qgf4l8n9mtusob" TEXT="Informasi tentang Oxymeter dan Fight Happy Hypoxia bisa dipelajari dari [video](https://www.youtube.com/watch?v=Dkot4GFqflM) ini.">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="3ra2jpk50doets25jpa3p0nlsk" TEXT="pattern">
+                        <node ID="1ssfecm9cuarkau2f06nvaako4" TEXT="pengetahuan oxymeter"/>
+                        <node ID="2ihv8r6mb2s2jvtiihninfq4r7" TEXT="happy hypoxia"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="10ra8e3ijf245hfj2265nmdahv" TEXT="Stres dalam sudut pandang yang &quot;membahagiakan&quot;">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="7bmbbvoejikps0tqj8p46955af" TEXT="Memahami [Stress dalam sudut pandang yang &quot;membahagikan&quot;](https://www.youtube.com/watch?v=-CLeA6YXrhs).">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="1fqifqha6n16r1qnc7dhgj9ag0" TEXT="pattern">
+                        <node ID="4l0pp189h5mqdvpuu8nn9b006d" TEXT="(stress|stres) yang membahagiakan"/>
+                        <node ID="70jmo2plrj6ipu3p8kvaojqj7c" TEXT="(stress|stres) dalam sudut pandang yang membahagiakan"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="5bmm8d10ttne5iq0iqkl6u5ons" TEXT="Manfaan Napas Diafragma">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="1qbtcnls5uaqms0mdpk1688dmv" TEXT="*Manfaat Napas Diafragma*&#10;&#10;Merangsang produksi Melatonin yang merupakan counter dari Cortisol.&#10;https://www.youtube.com/watch?v=GLgepWi2Rl0">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="04min158upv0ct7uik9i19i00m" TEXT="pattern">
+                        <node ID="3ui30tqks7v98k4odmgi1ff8h4" TEXT="manfaat napas diafragma"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="5nc7lgitd4crqd851l764ooc82" TEXT="Manfaat Napas Hidung-hidung">
+                    <node BACKGROUND_COLOR="#F2F2F2" ID="3pust4f7urodq33olnjv2kugei" TEXT="*Manfaat Napas Hidung-hidung*&#10;&#10;Merangsang produksi Nitric Oxide yang bersifat antivirus.&#10;https://www.youtube.com/watch?v=Ws5k4LMYN8s">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="33s7ftrt6adt74agoo1t5vthik" TEXT="pattern">
+                        <node ID="7c2a6p3n0qokpdeeuf003k29ev" TEXT="manfaat napas hidung"/>
+                    </node>
+                </node>
+            </node>
+        </node>
+        <node FOLDER="true" ID="7h8l02b0plihd7pjm99i6jr2ig" POSITION="left" TEXT="&#128222; Kontak">
+            <node BACKGROUND_COLOR="#F2F2F2" ID="28d0iaqle8rk1mr9vjprpl5aa6" TEXT="*Wisata Bugar*&#10;&#10;Wisata Bugar sudah hadir di beberapa kota di Indonesia seperti: Palembang, Makassar, Pontianak, DKI Jakarta, Bandung, Bogor, Cimahi, Cirebon, Tangerang, Bekasi, Yogyakarta, Semarang, Malang, Surabaya, Bali dan Papua.&#10;&#10;website: [wisatabugarofficial.com](https://www.wisatabugarofficial.com/)&#10;whatsapp: [0896.5173.4140](https://wa.me/6289651734140)&#10;instagram: [WisataBugarOfficial](https://www.instagram.com/WisataBugarOfficial/)&#10;">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node FOLDER="true" ID="3ht0jfh2hhm8edg9gob4d32e9k" TEXT="pattern">
+                <node ID="5hkrbstjk4knmabqbarb9a1aaq" TEXT="kontak wisata bugar"/>
+                <node ID="32mr4qb95455aj8m3smv922n47" TEXT="kontak wisatabugar"/>
+            </node>
+        </node>
+    </node>
+</map>
