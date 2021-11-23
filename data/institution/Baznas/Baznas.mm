0287f55f2eeeb799e2433e584ab490f0242bb56a
luri <luri@kioss.com>
2021-11-23 14:28:13 +0700

    institution: Baznas

---
+++ data/institution/Baznas/Baznas.mm
@@ -0,0 +1,270 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+    <node ID="b9aa22deba98b3b20c7ac8aca2" STYLE="bubble" TEXT="Baznas">
+        <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                *Baznas*
+
+Badan Amil Zakat Nasional (BAZNAS) merupakan badan resmi dan satu-satunya yang dibentuk oleh pemerintah berdasarkan Keputusan Presiden RI No. 8 Tahun 2001 yang memiliki tugas dan fungsi menghimpun dan menyalurkan zakat, infaq, dan sedekah (ZIS) pada tingkat nasional. Lahirnya Undang-Undang Nomor 23 Tahun 2011 tentang Pengelolaan Zakat semakin mengukuhkan peran BAZNAS sebagai lembaga yang berwenang melakukan pengelolaan zakat secara nasional. Dalam UU tersebut, BAZNAS dinyatakan sebagai lembaga pemerintah nonstruktural yang bersifat mandiri dan bertanggung jawab kepada Presiden melalui Menteri Agama.
+
+baznas.go.id
+            </text>
+        </hook>
+        <node FOLDER="true" ID="37srgadb0uf6avt6n1bum4ujr9" POSITION="right" TEXT="Zakat">
+            <node BACKGROUND_COLOR="#D9D9D9" ID="1gmgaqh1kn5dkas3o43jt54hje" TEXT="*Bayar Zakat*&#13;&#10;&#13;&#10;Kini Anda dapat bayar zakat secara online dengan menggunakan berbagai metode pembayaran. Tunaikan zakat lebih mudah dimana pun dan kapan pun melalui layanan Baznas.&#13;&#10;">
+                <font NAME="SansSerif" SIZE="12"/>
+            </node>
+            <node ID="00c7vek9mnqqfn3cljuckus0ru" TEXT="action type">
+                <node ID="0ojff0dnc2um7c6eau56ag45eb" TEXT="button"/>
+            </node>
+            <node ID="3lrubpqfrrv7sc0531ku15nc0f" TEXT="action">
+                <node ID="4bp97fuq5qr8h1pmp2ei0l3k2c" TEXT="Pembayaran">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="26iodj9ia8s1avqappna981h51" TEXT="*Pembayaran Zakat*&#13;&#10;&#13;&#10;Pembayaran zakat dapat dilakukan langsung melalui situs [Baznas](https://baznas.go.id/bayarzakat)&#13;&#10;">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="1117qop3id6nc02f1b557otgta" TEXT="action type">
+                        <node ID="67egfh493sid69ttda2ca8bnio" TEXT="button"/>
+                    </node>
+                    <node ID="2rbei3ic7s7lqhcngut8r1913a" TEXT="action">
+                        <node ID="667mrs077sk1oftt2lbklvsd3f" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=bayar zakat
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="2psfq6jj8n6frao8ciput9bpgt" TEXT="Konfirmasi">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="17u2mq6ark5gtng790qrt8eq64" TEXT="*Konfirmasi Pembayaran Zakat*&#13;&#10;&#13;&#10;Konfirmasi pembayaran zakat dapat dilakukan langsung melalui situs [Baznas](https://baznas.go.id/konfirmasi)&#13;&#10;Terima Kasih atas Zakat, Infak, Sedekah, Kurban yang ditunaikan melalui BAZNAS.&#13;&#10;_Semoga Allah memberikan pahala kepadamu pada barang yang engkau berikan dan semoga Allah memberkahimu dalam harta-harta yang masih engkau sisakan semoga pula menjadikannya sebagai pembersih (dosa) bagimu._&#13;&#10;">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="3dn22sreqd4va6aqnttp0keot8" TEXT="action type">
+                        <node ID="6e0i1pa2m34rs7nfq5lrb1lv1l" TEXT="button"/>
+                    </node>
+                    <node ID="6rgtl7bqco8rp4qcmt9s0c2mkg" TEXT="action">
+                        <node ID="1ga8bpfunud5hdqc0bc32q2up7" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=bayar zakat
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                    <node ID="3op65up4qmi92h9cf99ag0872u" TEXT="pattern">
+                        <node ID="3o2e2n5v5hu7u700r712dfqtfr" TEXT="konfirmasi pembayaran zakat"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="2orosvn109gsu3kifk48drogn1" TEXT="Rekening">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="2cq2dq1jp4is8171ikcj5k9999" TEXT="*Rekening Pembayaran*&#13;&#10;&#13;&#10;Rekening disesuaikan terhadap jenis dana (zakat, infak/sedekah, program tematik atau kurban). Pembayaran bisa dilakukan melalui transfer dari ATM, M-banking, i-banking, SMS-banking, dan atau teller bank. Simpan bukti transfer, kemudian segeralah melakukan konfirmasi pembayaran.&#13;&#10;&#13;&#10;https://baznas.go.id/rekening&#13;&#10;">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="6eqpu343rdo1cmn02huv2tf5vk" TEXT="action type">
+                        <node ID="1fctn845553tmu3n7c3og3qfuj" TEXT="button"/>
+                    </node>
+                    <node ID="0fq5mhchl8f1qiilb3g1mm0b86" TEXT="action">
+                        <node ID="7si5md9gar45sev14gci0bq8ed" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=bayar zakat
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="5hhab4islmc03eht11u77e3ap4" TEXT="Tentang Zakat">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="44u0sn5mtsmhrbh3ukqe9nsan8" TEXT="....&#13;&#10;catatan: detil informasi tentang zakat fitrah, infak, sedekah, dan fidyah. Serta zakat maal seperti zakat perusahaan, zakat penghasilan, zakat emas dan perak, zakat perdagangan, zakat saham dan zakat reksadana.">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="72gcsfgo08pipc48q330bcouo9" TEXT="action type">
+                        <node ID="46e8u7s42cno06f5b6s9jk4c53" TEXT="button"/>
+                    </node>
+                    <node ID="6um0itmj76fsekfc4v3nu4eb2j" TEXT="action">
+                        <node ID="6fcuequs2d6nkf7gd0je47m9a7" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=bayar zakat
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                </node>
+            </node>
+            <node ID="00a5umnu3re6j38hs2vaihr3oj" TEXT="pattern">
+                <node ID="470m44o6oau9nd0fsdjfohng6l" TEXT="bayar zakat"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="1egv3l1oul4f2423mv06gq6mnm" POSITION="right" TEXT="Layanan Publik">
+            <node BACKGROUND_COLOR="#D9D9D9" ID="43ij97drr9t34adf9end6lvk24" TEXT="*Badan Amil Zakat Nasional (BAZNAS)* memiliki beberapa layanan untuk memudahkan hubungan dengan masyarakat.">
+                <font NAME="SansSerif" SIZE="12"/>
+            </node>
+            <node ID="5up6jl3ehg6slarert6d2av2is" TEXT="action type">
+                <node ID="55pl1sumcfvpdn0osdqmfapatv" TEXT="button"/>
+            </node>
+            <node FOLDER="true" ID="37drq27cjdnh243pjefp7fp74i" TEXT="action">
+                <node ID="15h3gdatbt70b5cg2aa3qnemes" TEXT="Pengelola Informasi dan Dokumentasi">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="3dnat524oddlof9s78708v0549" TEXT="*PPID (Pejabat Pengelola Informasi dan Dokumentasi)*&#13;&#10;&#13;&#10;Pejabat Pengelola Informasi dan Dokumentasi (PPID) menjalankan tugas dan fungsi BAZNAS dalam hal pengelolaan informasi dan kearsipan.&#13;&#10;&#13;&#10;Sebagaimana yang kita pahami bersama bahwa BAZNAS telah mendapatkan amanah dari pemerintah untuk melaksanakan amanah pengelolaan zakat secara nasional yang tertuang dalam Undang-undang nomor 23 tahun 2011 tentang zakat.&#13;&#10;&#13;&#10;Informasi lebih lengkap tentang hal ini bisa Anda dapatkan dari [Situs PPID](https://pid.baznas.go.id/).&#13;&#10;&#13;&#10;">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="61kcv2c5qu82gps67srml35u14" TEXT="action type">
+                        <node ID="3kt6p4prpc2555vuuhrtslaf5f" TEXT="button"/>
+                    </node>
+                    <node ID="111f83hgtvq6h9slu7vp1rvorh" TEXT="action">
+                        <node ID="46571ucrot1qk1cus5v3e6viul" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=info baznas
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                </node>
+                <node ID="62r0j78hobhkeqvjhra9188qpm" TEXT="Laporan Keuangan">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="2998q4eqrsvdvgrbsj8p4cn5sv" TEXT="*Laporan Keuangan BAZNAS 2020 Audited*&#13;&#10;&#13;&#10;Laporan keuangan BAZNAS tahun 2020 dan tahun-tahun yang lain bisa dilihat dari [tautan ini](https://baznas.go.id/keuangan/2020).&#13;&#10;&#13;&#10;">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="7bo1rileerkkl4hp1hn8es7add" TEXT="action type">
+                        <node ID="2lvhrnqlnoc8gs7pa8snueqlrb" TEXT="button"/>
+                    </node>
+                    <node ID="2amiosha3g2d1lbof9cokdt9km" TEXT="action">
+                        <node ID="072k0nqh54io46po9cb561f2je" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=info baznas
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                    <node ID="5a4qjcm602dhleqf7pdpuplc8e" TEXT="pattern">
+                        <node ID="50eb0p7t22v0oldmok38foilm8" TEXT="laporan keuangan baznas"/>
+                    </node>
+                </node>
+            </node>
+            <node ID="6cvvq6auj0j2o9k2athv50tuj9" TEXT="pattern">
+                <node ID="5uicksb6ckdvttfaqald1f8uob" TEXT="layanan public baznas"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="41jmrul4t12e1j5oceio77bkd1" POSITION="right" TEXT="Program">
+            <node BACKGROUND_COLOR="#D9D9D9" ID="1rorganbarc47ii9addkn61pof" TEXT="*Program BAZNAS*&#13;&#10;&#13;&#10;BAZNAS memiliki beberapa program kemanusiaan dalam berbagai bidang, seperti:&#13;&#10;&#13;&#10;Kemanusiaan&#13;&#10;- BAZNAS Tanggap Bencana&#13;&#10;- Layanan Aktif&#13;&#10;Pendidikan&#13;&#10;- Lembaga Beasiswa BAZNAS&#13;&#10;- Sekolah Cendikia BAZNAS&#13;&#10;Kesehatan&#13;&#10;- Rumah Sehat BAZNAS&#13;&#10;Dakwah&#13;&#10;- Muallaf Center BAZNAS&#13;&#10;Ekonomi&#13;&#10;- Pemberdayaan Peternak&#13;&#10;- Pengembangan Ekonomi&#13;&#10;- Zakat Community Development&#13;&#10;- BAZNAS Microfinance&#13;&#10;&#13;&#10;">
+                <font NAME="SansSerif" SIZE="12"/>
+            </node>
+            <node ID="79869vtd07m9okcg7841iajncc" TEXT="action type">
+                <node ID="3nfidihk79mrelm9eqk3tl3qkr" TEXT="button"/>
+            </node>
+            <node ID="0qs644du1cfhhpj43k47o63jfu" TEXT="action">
+                <node ID="1e330hmb2nv5hllc6sksqjqsld" TEXT="Kembali">
+                    <hook NAME="accessories/plugins/NodeNote.properties">
+                        <text>
+                            callback=bayar zakat
+                        </text>
+                    </hook>
+                </node>
+            </node>
+            <node ID="4a6bvgr2apkv7j8ieci9qnk833" TEXT="pattern">
+                <node ID="6g5s30s4hqkd66s5kaehu2pl8m" TEXT="program baznas"/>
+            </node>
+        </node>
+        <node ID="1pi7uu281trfe2074p4vh7euu0" POSITION="left" TEXT="Profil">
+            <node BACKGROUND_COLOR="#D9D9D9" ID="5pemsg3pq4f3oq81t9a82cvjpl" TEXT="*Badan Amil Zakat Nasional (BAZNAS)*&#13;&#10;&#13;&#10;Badan Amil Zakat Nasional (BAZNAS) merupakan badan resmi dan satu-satunya yang dibentuk oleh pemerintah berdasarkan Keputusan Presiden RI No. 8 Tahun 2001 yang memiliki tugas dan fungsi menghimpun dan menyalurkan zakat, infaq, dan sedekah (ZIS) pada tingkat nasional. Lahirnya Undang-Undang Nomor 23 Tahun 2011 tentang Pengelolaan Zakat semakin mengukuhkan peran BAZNAS sebagai lembaga yang berwenang melakukan pengelolaan zakat secara nasional. Dalam UU tersebut, BAZNAS dinyatakan sebagai lembaga pemerintah nonstruktural yang bersifat mandiri dan bertanggung jawab kepada Presiden melalui Menteri Agama.&#13;&#10;&#13;&#10;Dengan demikian, BAZNAS bersama Pemerintah bertanggung jawab untuk mengawal pengelolaan zakat yang berasaskan: syariat Islam, amanah, kemanfaatan, keadilan, kepastian hukum, terintegrasi dan akuntabilitas.&#13;&#10;&#13;&#10;">
+                <font NAME="SansSerif" SIZE="12"/>
+            </node>
+            <node ID="2cj8ej8d6fr48o6ll4vi6vmhvg" TEXT="action type">
+                <node ID="5fl35vm0m613t8mgcqdg31n5uk" TEXT="button"/>
+            </node>
+            <node FOLDER="true" ID="72i52etdisfc4r61gr7l35i7dn" TEXT="action">
+                <node ID="3aje7unqp33joporvde7vv1rci" TEXT="Visi">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="2dusebc8501uau0590totdopj2" TEXT="*Vizi (BAZNAS)*&#13;&#10;&#13;&#10;....">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="5q7540gqb9mva0htgk58c2mtpa" TEXT="action type">
+                        <node ID="4nfg5t91jtu1c2gjbu47pmbdga" TEXT="button"/>
+                    </node>
+                    <node ID="4k1q4q0eb7u1dc9nien5eisnsd" TEXT="action">
+                        <node ID="70ndq8046cs2bola8u75bnk9s6" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=info baznas
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                </node>
+                <node ID="6e3vu03hftu3v3r7vfb2ip23j0" TEXT="Misi">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="099qbq5fvdqc4ob1u4kd98rg2f" TEXT="*Misi (BAZNAS)*&#13;&#10;&#13;&#10;....">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="2a51336f1kfgssvncs5ani06h5" TEXT="action type">
+                        <node ID="342agudltui8ie2lg7i20o3vm8" TEXT="button"/>
+                    </node>
+                    <node ID="6p5q1t10bv5a5ccu7k288s1j4f" TEXT="action">
+                        <node ID="1ouri1ns3jpvqc5l02rtfo18ms" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=info baznas
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                </node>
+                <node ID="21nome33vsja81n6bl9j6dnntk" TEXT="Tujuan">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="21raevh57cc8n5tjppufpaed03" TEXT="*Tujuan (BAZNAS)*&#13;&#10;&#13;&#10;....">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="7jt6h426cv0jj0rkt77siq3ove" TEXT="action type">
+                        <node ID="44aekr24h0urvgqmh1ilk5qvf5" TEXT="button"/>
+                    </node>
+                    <node ID="7sigas6m3dn8dqjt4otbufo7pp" TEXT="action">
+                        <node ID="4g4j7161vlaqog16iiigg49f04" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=info baznas
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                </node>
+                <node ID="0mtr13corfrr7dlunh7t3bg5k8" TEXT="Sasaran">
+                    <node BACKGROUND_COLOR="#D9D9D9" ID="4v4m16rcqks13drnql0jn48jdr" TEXT="*Sasaran (BAZNAS)*&#13;&#10;&#13;&#10;....">
+                        <font NAME="SansSerif" SIZE="12"/>
+                    </node>
+                    <node ID="0nb2vcc9pbmfs5pn93ndrinplh" TEXT="action type">
+                        <node ID="48dgov5jtd8r8vcn0haab0ioj5" TEXT="button"/>
+                    </node>
+                    <node ID="63ofnefh8f5k66h4gb0nua8the" TEXT="action">
+                        <node ID="6sab8r7j5np5nuqndjvtk49tl2" TEXT="Kembali">
+                            <hook NAME="accessories/plugins/NodeNote.properties">
+                                <text>
+                                    callback=info baznas
+                                </text>
+                            </hook>
+                        </node>
+                    </node>
+                </node>
+            </node>
+            <node ID="4sucinqel63r4dpv5cho6iptmb" TEXT="pattern">
+                <node ID="3jksok6bvqhgei9b81q25435ea" TEXT="(profile|profil) baznas"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="4gn2ac33itmk8h96bounsr7kch" POSITION="left" TEXT="&#128222; Kontak">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="167csnr0bk05r4k05eqfvtc33d" TEXT="*Badan Amil Zakat Nasional (BAZNAS)*&#10;&#10;Alamat: Jl. Matraman Raya No.134,&#10;Kb. Manggis, Kec. Matraman,&#10;Jakarta 13150&#10;https://baznas.go.id&#10;&#10;Email: baznas@baznas.go.id&#10;Telepon: +6221-22897983&#10;&#10;Konfirmasi Donasi: [087877373555](https://wa.me/6287877373555)&#10;">
+                <font NAME="SansSerif" SIZE="12"/>
+            </node>
+            <node ID="4o0l83mffebpn7u2nsnrk0ua70" TEXT="Action Type">
+                <node FOLDER="true" ID="2de67lr5uq3nn772koj0ap0rmn" TEXT="button"/>
+            </node>
+            <node ID="6mf9cakkgeh768d0d531j3groj" TEXT="Action">
+                <node FOLDER="true" ID="2pqqvct983v50i7n8gmvtsp4vr" TEXT="&#127962; Kembali">
+                    <hook NAME="accessories/plugins/NodeNote.properties">
+                        <text>
+                            callback=info baznas
+                        </text>
+                    </hook>
+                </node>
+            </node>
+            <node ID="6pk3gi49evoobmarr8uho9u150" TEXT="pattern">
+                <node FOLDER="true" ID="199q3gm9ia1lqesoeqg3hen5o2" TEXT="(contact|kontak|wa) baznas"/>
+                <node FOLDER="true" ID="48fs9qoof87au4ncqr96i3m8h3" TEXT="baznas (contact|kontak)"/>
+            </node>
+        </node>
+    </node>
+</map>
