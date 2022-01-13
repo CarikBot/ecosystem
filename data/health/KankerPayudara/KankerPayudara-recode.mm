a7d039b8f9d067d154628fd54ef11a8a7c171510
Carik Ecosystem Builder <carik.bot@gmail.com>
2022-01-13 23:00:05 +0700

    Ecosystem Builder: health.KankerPayudara

---
+++ data/health/KankerPayudara/KankerPayudara-recode.mm
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+  <node ID="62ura2p7lvlji4gkg1f40ah8k1" STYLE="bubble" TEXT="KankerPayudara">
+    <hook NAME="accessories/plugins/NodeNote.properties">
+      <text>
+                *Kanker Payudara*
+
+Kanker payudara merupakan suatu jenis tumor ganas yang berkembang pada sel-sel payudara. Kanker ini dapat tumbuh jika terjadi pertumbuhan yang abnormal dari sel-sel pada payudara. Sel-sel tersebut membelah diri lebih cepat dari sel normal dan berakumulasi, yang kemudian membentuk benjolan atau massa.
+
+Kanker payudara dapat terjadi pada wanita dan jarang pada pria.
+Gejala kanker payudara termasuk benjolan di payudara, keluarnya cairan berdarah dari puting, dan perubahan bentuk atau tekstur puting atau payudara. Penanganan tergantung pada stadium kanker. Penanganan dapat terdiri dari kemoterapi, radiasi, dan operasi.
+            </text>
+    </hook>
+    <node FOLDER="true" ID="72vmoiu1ur5ljoukkduvaod6nm" POSITION="right" TEXT="Gejala">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="2cqm1gi8q1hla0drontlvv0tdf" TEXT="*Gejala Kanker Payudara*&#13;&#10;&#13;&#10;Pada stadium dini, kanker payudara dapat tidak menunjukkan gejala tertentu. Oleh karena itu, sangat penting untuk melakukan SADARI atau periksa payudara sendiri setiap bulan, 10 hari setelah masa haid berakhir. Raba dengan teliti searah jarum jam payudara untuk mendeteksi adanya benjolan atau perubahan pada payudara. Beberapa gejala kanker payudara, antara lain:&#13;&#10;&#13;&#10;- Benjolan atau pengerasan pada payudara yang berbeda dari jaringan sekitar.&#13;&#10;- Darah keluar dari puting payudara.&#13;&#10;- Kemerahan atau pembesaran pori-pori kulit payudara yang menyerupai kulit jeruk.&#13;&#10;- Nyeri dan pembengkakan pada payudara.&#13;&#10;- Pengelupasan kulit di sekitar puting payudara.&#13;&#10;- Perubahan pada kulit payudara, seperti cekungan.&#13;&#10;- Perubahan ukuran, bentuk, atau tampilan dari payudara.&#13;&#10;- Puting tertarik masuk (retraksi atau inversi) ke dalam.&#13;&#10;- Benjolan atau pembengkakan di bawah ketiak.&#13;&#10;&#13;&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="6te7miqms4m14hh3u7ll13oaua" TEXT="Action Type">
+        <node ID="6dfgud5iku69c2103ec63g2eje" TEXT="button"/>
+      </node>
+      <node ID="6empsn280c128dfsl3689cottm" TEXT="Action">
+        <node ID="2bcevcm130j3r5gk3t1ielvf54" TEXT="Kembali">
+          <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                            callback=info kanker payudara
+                        </text>
+          </hook>
+        </node>
+      </node>
+      <node ID="5mdlttp3ktr19gb9b0cdkvajs4" TEXT="pattern">
+        <node ID="44u44ahocihlkg4en6hm6r4ses" TEXT="gejala kanker payudara"/>
+      </node>
+    </node>
+    <node FOLDER="true" ID="3uc571831vlmcoav8c0cau5jia" POSITION="right" TEXT="Penyebab">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="3f57a3j8rdgq5vcc00jeg4icb0" TEXT="*Penyebab dan Faktor Risiko Kanker Payudara*&#10;&#10;Kanker payudara terjadi akibat pertumbuhan abnormal dari sel-sel pada payudara. Pertumbuhan abnormal ini diduga disebabkan oleh mutasi gen yang diturunkan secara genetik. Selain itu, terdapat beberapa faktor risiko yang diduga menjadi pemicu kanker payudara, yaitu:&#10;&#10;- Jenis kelamin wanita jauh lebih tinggi dibandingkan pria.&#10;- Usia yang bertambah, paling banyak pada usia di atas 50 tahun.&#10;- Belum pernah hamil sebelumnya.&#10;- Kebiasaan merokok atau minum minuman beralkohol.&#10;- Kelebihan berat badan atau obesitas.&#10;- Mulai menopause pada usia lebih tua, yaitu setelah usia 55 tahun.&#10;- Mulai menstruasi sebelum usia 12 tahun.&#10;- Penggunaan alat kontrasepsi hormon dan terapi hormon setelah menopause.&#10;- Riwayat kanker payudara pada diri sendiri pada salah satu payudara.&#10;- Riwayat kanker payudara pada nenek, ibu, tante, adik, kakak, atau anak sekandung.&#10;- Riwayat terpapar dengan radiasi.&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="21a58voefki1ms5vmd6fmrf0ds" TEXT="Action Type">
+        <node ID="2ac8lu3dtgu8jtaiumgl9bqqkv" TEXT="button"/>
+      </node>
+      <node ID="1hp5m8703f0ounsr53paon1rmu" TEXT="Action">
+        <node ID="5nc7lgitd4crqd851l764ooc82" TEXT="Kembali">
+          <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                            callback=info kanker payudara
+                        </text>
+          </hook>
+        </node>
+      </node>
+      <node ID="4lub2v14ci1jlp17mfuol7of7k" TEXT="pattern">
+        <node ID="240l5t5pcs7to1cl963ongu741" TEXT="penyebab kanker payudara"/>
+      </node>
+    </node>
+    <node FOLDER="true" ID="3l8s76g3goc0pbnhqp90o9r21o" POSITION="right" TEXT="Jenis">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="7imvsbtkk6ibmpdv8f34gm4mp1" TEXT="*Jenis Kanker Payudara*&#10;&#10;Ada beberapa jenis kanker payudara yang terbagi menjadi dua tipe yang berbeda, yaitu invasive dan non-invasive. Kanker payudara invasive terjadi ketika sel kanker telah menyebar ke bagian lain payudara. Sedangkan kanker payudara non-invasive, merupakan kondisi sel kanker belum menyebar dari jaringan aslinya. Ada beberapa jenis kanker payudara yang sering terjadi:&#10;&#10;- Ductal Carcinoma In Situ;&#10;- Lobular Carnicoma In Situ;&#10;- Invasive Ductal Carcinoma; dan&#10;- Invasive Lobular Carcinoma.&#10;&#10;Sementara, beberapa jenis kanker payudara yang jarang terjadi adalah paget disease of the breast, angiosarcoma, hingga phyllodes tumor. Mengetahui jenis kanker payudara yang kamu alami adalah hal yang cukup penting. Hal ini akan menentukan pengobatan dan perawatan yang perlu kamu lakukan untuk mengatasi penyakit ini.">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="7abrmrtesqdv5ieft50vl2420c" TEXT="Action Type">
+        <node ID="3g0d0cp50g3h383osgl0sc4th8" TEXT="button"/>
+      </node>
+      <node ID="450lud149fnmobi889dbbe9kv1" TEXT="Action">
+        <node ID="4q344gom65tthkjj74k7ru2mpb" TEXT="Kembali">
+          <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                            callback=info kanker payudara
+                        </text>
+          </hook>
+        </node>
+      </node>
+      <node ID="00toct0g0n5f4so7kml4og0is5" TEXT="pattern">
+        <node ID="7ifg9am036m51pv2emkh6o4a70" TEXT="jenis kanker payudara"/>
+      </node>
+    </node>
+    <node FOLDER="true" ID="07nlkaumd98gvkgh1q4a086q7i" POSITION="right" TEXT="Diagnosis">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="7e9j4d23dqdamnvluic4m9ado4" TEXT="*Diagnosis Kanker Payudara*&#10;&#10;Dokter akan mendiagnosis kanker payudara dengan melakukan wawancara medis lengkap, pemeriksaan fisik langsung untuk mendeteksi perubahan pada payudara serta kelenjar getah bening pada ketiak, serta pemeriksaan penunjang, seperti:&#10;&#10;- Mammogram atau foto payudara, untuk mendeteksi kelainan pada payudara.&#10;- Ultrasonografi (USG) payudara, untuk menentukan benjolan payudara berupa massa padat atau kista yang berisi cairan.&#10;- Biopsi dengan pengambilan sampel jaringan, untuk diperiksa di laboratorium dan menentukan sel yang diperiksa bersifat jinak atau ganas.&#10;- Computerized Tomography scan (CT scan) dan Magnetic Resonance Imaging (MRI), untuk menentukan ukuran serta penyebaran dari kanker payudara.&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="1jafb08tn81fm1dmapgk25ts1t" TEXT="Action Type">
+        <node ID="7vb1rs04050j2shrr3s6so4sak" TEXT="button"/>
+      </node>
+      <node ID="3svavv3nvglkas2qisjl46hmte" TEXT="Action">
+        <node ID="5pr69c4snjgjoujjcts6kobvbr" TEXT="Kembali">
+          <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                            callback=info kanker payudara
+                        </text>
+          </hook>
+        </node>
+      </node>
+      <node ID="10jli1u80hkqs7q09ar088g5ce" TEXT="pattern">
+        <node ID="7re2an2eu8v2rjb6ffvctvaq16" TEXT="(diagnosis|diagnosa) kanker payudara"/>
+      </node>
+    </node>
+    <node FOLDER="true" ID="4ocqu0ljd9foe24kcqktimm31a" POSITION="left" TEXT="Pencegahan">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="3j6eqmjoirac1uv3u2r9b3oa6d" TEXT="*Pencegahan Kanker Payudara*&#10;&#10;Beberapa upaya pencegahan kanker payudara, antara lain:&#10;&#10;- Berolahraga secara rutin.&#10;- Cukup istirahat.&#10;- Menyusui anak hingga berusia dua tahun.&#10;- Pemeriksaan rutin dan teliti dengan SADARI.&#10;- Pengelolaan stres yang baik.&#10;- Pola makan gizi seimbang.&#10;- Tidak merokok atau minum minuman beralkohol.">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="2guajug4j3va37sqm1sgr1uvdm" TEXT="Action Type">
+        <node ID="0gm2fhucqrdvqcdluhh8q08nhf" TEXT="button"/>
+      </node>
+      <node ID="69ov28sai196ieluonefl13g9e" TEXT="Action">
+        <node ID="2narkfnmfjgmhenhv23kgvopma" TEXT="Kembali">
+          <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                            callback=info kanker payudara
+                        </text>
+          </hook>
+        </node>
+      </node>
+      <node ID="4mmcdejf5apj666omm0ratscon" TEXT="pattern">
+        <node ID="18797t2ngsajlelfj6kf08n5ff" TEXT="pencegahan kanker payudara"/>
+      </node>
+    </node>
+    <node FOLDER="true" ID="0a8uks3kh25qrded772p43ai1o" POSITION="left" TEXT="Pengobatan">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="5umddsp30g4krhg37urq4uaa45" TEXT="*Pengobatan Kanker Payudara*&#10;&#10;Penanganan kanker payudara bergantung pada jenis kanker yang dialami, stadium kanker, ukuran massa, serta sensitivitas sel kanker terhadap hormon. Dokter akan menentukan terapi yang paling sesuai dengan keadaan penderita berdasarkan hal-hal tersebut. Beberapa pilihan pengobatan pada kanker payudara, antara lain:&#10;&#10;- Pembedahan, yang meliputi pengangkatan kanker atau benjolan (lumpektomi), pengangkatan seluruh payudara (mastektomi), pengangkatan jumlah terbatas dari kelenjar limfe (sentinel node biopsy), atau pengangkatan beberapa kelenjar limfe (axillary lymph node dissection).&#10;- Radioterapi, yang dilakukan dengan menggunakan energi sinar X dan proton, untuk mematikan sel-sel kanker.&#10;- Kemoterapi, yang dilakukan dengan menggunakan obat-obatan tertentu, untuk mematikan sel kanker.&#10;- Terapi penghambat hormon, jika kanker diketahui sensitif terhadap hormon estrogen atau progesteron.&#10;&#10;Umumnya, pengidap akan menjalani prosedur pembedahan untuk kanker payudara dan mendapatkan penanganan lain sebelum dan/atau sesudah pembedahan, seperti kemoterapi, terapi hormonal, atau terapi radiasi.&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="7cun40ap2fk699f87kpegf1jei" TEXT="Action Type">
+        <node ID="6kumgl5am2kha8c9f3k43aiqqf" TEXT="button"/>
+      </node>
+      <node ID="0tkor6m2265dijfm5elh02ib4n" TEXT="Action">
+        <node ID="34af6c201olteam4uvhbpmi25e" TEXT="Bahaya Kemo">
+          <node BACKGROUND_COLOR="#F2F2F2" ID="3tpbr8op595m911562rrtnu1r4" TEXT="*Efek Samping Kemoterapi*&#10;&#10;Kemoterapi adalah jenis perawatan untuk mengobati kanker, dengan menggunakan obat-obatan, yang bisa mematikan sel kanker, pada tubuh pasiennya. Kemoterapi tersedia dalam berbagai bentuk, mulai dari pil atau cairan yang bisa langsung ditelan, cairan yang diinfus langsung ke dalam organ, hingga suntikan.&#10;&#10;1. Rambut rontok&#10;2. Melemahkan sistem kekebalan tubuh&#10;3. Gangguan saraf&#10;4. Gangguan pernapasan&#10;5. Mengganggu sistem pencernaan&#10;6. Ruam kulit&#10;7. Mual dan muntah&#10;8. Mengganggu sistem reproduksi&#10;&#10;Dalam kasus yang sangat jarang terjadi, efek samping kemoterapi di atas, bisa bersifat permanen pada pasien kanker. Contohnya, kerusakan saraf permanen, yang dapat menimbulkan rasa kesemutan kronis di tangan dan kaki.&#10;">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+          <node ID="0f44i8i2rgaganlu51bupsnpno" TEXT="Action Type">
+            <node ID="03nqnc00riq8c0b6qs68h4h1p1" TEXT="button"/>
+          </node>
+          <node ID="5ru06mn97nc1ptaon4k0kp2hkt" TEXT="Action">
+            <node ID="28ua8jgead5bonhub4i5r6j12k" TEXT="Kembali">
+              <hook NAME="accessories/plugins/NodeNote.properties">
+                <text>
+                                    callback=info kanker payudara
+                                </text>
+              </hook>
+            </node>
+          </node>
+          <node ID="1gcekd6ml3ost0jm1rbkbiefrj" TEXT="pattern">
+            <node ID="1ad8g22n20iqt8r3cs5s488sv8" TEXT="bahaya (kemo|kemoterapi)"/>
+            <node ID="15ff5j22ull7g9t1u8r46m8k0f" TEXT="efek samping (kemo|kemoterapi)"/>
+          </node>
+        </node>
+        <node ID="02h5q95b08pcln625226ohcj45" TEXT="Kembali">
+          <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                            callback=info kanker payudara
+                        </text>
+          </hook>
+        </node>
+      </node>
+      <node ID="6im2cqcc1aa6sitfua88re6b0j" TEXT="pattern">
+        <node ID="5fp0vco3ceiuluhvnfkevgvio0" TEXT="pengobatan kanker payudara"/>
+      </node>
+    </node>
+  </node>
+</map>
