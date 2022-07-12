364eac35610dedf33ddbabdb7667236561eb10b2
Carik Ecosystem Builder <carik.bot@gmail.com>
2022-07-12 07:19:58 +0700

    Ecosystem Builder: finance.ksp212,main.CarikBot,partner.Taqmir

---
+++ data/partner/Taqmir/Taqmir-recode.mm
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+  <node ID="62ura2p7lvlji4gkg1f40ah8k1" STYLE="bubble" TEXT="Taqmir">
+    <hook NAME="accessories/plugins/NodeNote.properties">
+      <text>
+                Selamat datang di layanan chat bot *Taqmir*
+
+Taqmir mengadirkan Transformasi Digital untuk Masjid.
+Memudahkan pengurus untuk mengelola masjid supaya dapat memberikan pelayanan terbaik bagi jamaah. Gratis Website untuk Masjid, menerima donasi(infaq/sadaqah) diproses Bank Syariah Indonesia, langsung terkoneksi ke dashboard pengurus.
+
+Silakan pilih menu layanan ini.
+            </text>
+    </hook>
+    <node ID="4jj0ohci9oaqeb8ou19g73tju3" POSITION="right" TEXT="📝 Pendaftaran">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="0ec883pjp7urqpj4tdi0soth0k" TEXT="*Pendaftaran TAQMIR*&#10;&#10;Aplikasi TAQMIR terbuka bagi semua masjid baik masjid raya, masjid agung, masjid jami, masjid bersejarah, masjid umum, atau jenis masjid lainnya serta termasuk mushola. Yang berhak mendaftarkan masjid adalah Dewan Kemakmuran Masjid/Dewan Pengelola Masjid/Dewan Takmir Masjid/ atau sebutan lain yang mengacu pada pengelola masjid dengan mengisi form pendaftaran.&#10;&#10;[Daftarkan Segera](https://taqmir.com/docs/website-masjid/)&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node FOLDER="true" ID="0nhg465n83idegdp1ocqbfknco" TEXT="pattern">
+        <node ID="2pqj15pc4hnrh99fu02vgg3eca" TEXT="(pendaftaran|daftar) taqmir"/>
+      </node>
+    </node>
+    <node ID="299h6h2gi48tljinj2mqf2t344" POSITION="right" TEXT="🕌 Cari Masjid">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="5kou6q77h20j3j7lehmfln1326" TEXT="*Pencarian Masjid*&#13;&#10;&#13;&#10;Untuk mencari informasi masjid, silakan ketik dengan format ini:&#13;&#10;```CARI MASJID DI [AREA]```&#13;&#10;&#13;&#10;Misal:&#13;&#10;```CARI MASJID DI SEMARANG```&#13;&#10;&#13;&#10;&#13;&#10;_(Masih dalam development.)_">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node FOLDER="true" ID="1192dis74qhipdqqrso869j81d" TEXT="pattern">
+        <node ID="57g741o2kqufkn5vbvuuc6va34" TEXT="cari masjid"/>
+      </node>
+    </node>
+    <node ID="6p8c9llkd5bgqgi6003b2d4v81" POSITION="right" TEXT="❓ FAQ">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="2u41uvqcaob1ov3tv8o2dgt82o" TEXT="*FAQs / HELP CENTER*&#10;&#10;Berikut ini adalah pusat bantuan informasi aplikasi TAQMIR.&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="46vl5oovp1lc2odo5p9ha6akpt" TEXT="Action Type">
+        <node FOLDER="true" ID="3pr6tla4knad1jlt3lir7bi6eq" TEXT="button"/>
+      </node>
+      <node ID="66oklic6iipgr5tvfu3vi766vc" TEXT="Action">
+        <node FOLDER="true" ID="2t6qoeonnr0l7kmplj97hhjks4" TEXT="Aplikasi Mobile">
+          <node BACKGROUND_COLOR="#F2F2F2" ID="7lo64rdv8d5aa9798vm7qdb62b" TEXT="*Aplikasi Mobile*&#10;&#10;Informasi bantuan tentang:&#10;- Publikasi Keuangan&#10;- Langkah Pendaftaran TAQMIR.com&#10;- Mendaftar sebagai jamaah&#10;&#10;[Selengkapnya](https://taqmir.com/docs-category/aplikasi-mobile/)">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node FOLDER="true" ID="3105l8j29gk77d67crucokevr1" TEXT="Kelola Jamaah">
+          <node BACKGROUND_COLOR="#F2F2F2" ID="4rsu9i1p2t2muk20416krmb7i5" TEXT="*Kelola Jamaah*&#10;&#10;Informasi bantuan tentang:&#10;- Mengelola Data Jamaah&#10;- Mendaftar sebagai jamaah&#10;&#10;[Selengkapnya](https://taqmir.com/docs-category/kelola-jamaah/)">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node FOLDER="true" ID="0mbvstvhhgepjkjnasnfs1na8n" TEXT="Kelola Masjid">
+          <node BACKGROUND_COLOR="#F2F2F2" ID="34arup83bflla0qf60e3tu1tue" TEXT="*Kelola Masjid*&#10;&#10;Informasi bantuan tentang:&#10;- Website Masjid&#10;- Mengelola Data Masjid&#10;- Kegiatan Sholat Jum’at&#10;- Dokumentasi Masjid&#10;- Kelola Data Fasilitas Masjid&#10;- Kelola Kegiatan&#10;- Menambahkan Mubaligh&#10;- Menambahkan Rekening Masjid&#10;- Mendaftar sebagai jamaah&#10;&#10;[Selengkapnya](https://taqmir.com/docs-category/kelola-masjid/)">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node FOLDER="true" ID="7m83bfhc2fk10tnuokvqbr9e4r" TEXT="Pendaftaran">
+          <node BACKGROUND_COLOR="#F2F2F2" ID="66i0e8ibgjhdsc1jfs7dcue3qq" TEXT="*Pendaftaran*&#10;&#10;Informasi bantuan tentang:&#10;- Menambahkan Rekening Masjid&#10;- Langkah Pendaftaran TAQMIR.com&#10;- Mendaftar sebagai pengurus&#10;&#10;[Selengkapnya](https://taqmir.com/docs-category/pendaftaran/)">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node FOLDER="true" ID="4ks6rhqo1b25ais92nteofd3k5" TEXT="Keuangan">
+          <node BACKGROUND_COLOR="#F2F2F2" ID="377cvl9qv789eu6lhnm8otm60p" TEXT="*Keuangan*&#10;&#10;Informasi bantuan tentang:&#10;- Mengelola Keuangan Keluar&#10;- Mengelola Keuangan Masuk&#10;- Publikasi Keuangan&#10;- Menghitung Zakat&#10;- Menambahkan Rekening Masjid&#10;- Apa itu Chart Of Account (CoA)?&#10;&#10;[Selengkapnya](https://taqmir.com/docs-category/keuangan/)">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node FOLDER="true" ID="2uma5k1q1bsm0uje4dfn463hft" TEXT="Pengaturan">
+          <node BACKGROUND_COLOR="#F2F2F2" ID="3d4087icuqg250igu4u5k74kj1" TEXT="*Pengaturan*&#10;&#10;Informasi bantuan tentang:&#10;- Website Masjid&#10;- Publikasi Keuangan&#10;- Bagaimana mengubah foto profil&#10;- Mengubah logo masjid&#10;- Mengubah Password akun&#10;&#10;[Selengkapnya](https://taqmir.com/docs-category/pengaturan/)">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node FOLDER="true" ID="5tm02o9kdvl2r5ojj00mv1tkp0" TEXT="Tools">
+          <node BACKGROUND_COLOR="#F2F2F2" ID="37n3k3chv5bk9qk3b2q8icb83m" TEXT="*Tools*&#10;&#10;Informasi bantuan tentang:&#10;- Menghitung Zakat&#10;&#10;[Selengkapnya](https://taqmir.com/docs-category/tools/)">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+      </node>
+      <node FOLDER="true" ID="2p54f5gevjijccecea9trd2029" TEXT="pattern">
+        <node ID="0scovdmut4grh45gddggm8i93f" TEXT="faq taqmir"/>
+      </node>
+    </node>
+    <node ID="0tcvjqh8e8f7k5cosemsahbigo" POSITION="left" TEXT="Tentang Kami">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="1c6pbs6fm18u8lt7dhtpi40lk1" TEXT="*TAQMIR*&#10;Solusi Software Andal untuk manajemen masjid&#10;&#10;Taqmir mengadirkan Transformasi Digital untuk Masjid.&#10;Memudahkan pengurus untuk mengelola masjid supaya dapat memberikan pelayanan terbaik bagi jamaah. Gratis Website untuk Masjid, menerima donasi(infaq/sadaqah) diproses Bank Syariah Indonesia, langsung terkoneksi ke dashboard pengurus.&#10;&#10;Taqmir dikembangkan secara khusus untuk memenuhi kebutuhan administrasi dan keuangan Masjid di Indonesia. Meliputi manajemen informasi masjid, data jamaah, transaksi keuangan, perhitungan zakat dan wakaf, laporan kas, berita dan jadwal kegiatan. Taqmir tersedia gratis bagi semua masjid di indonesia yang membutuhkan sistem komputerisasi.&#10;&#10;Taqmir.com&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node FOLDER="true" ID="28ut9bofsbml971p1aclrmocd6" TEXT="pattern">
+        <node ID="6gko4mldid5214vetp6edohcus" TEXT="tentang taqmir"/>
+      </node>
+    </node>
+    <node FOLDER="true" ID="4j1j3kpfl7t3cstmoo13n1dkpv" POSITION="left" TEXT="📞 Kontak">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="26c5nitejfe9o8cmsl5dbs8p2n" TEXT="*TAQMIR*&#10;&#10;PT. GAIVO SOLUSI SISTEM INDONESIA&#10;Ruko Grand Boulevard U01A no 369 Citra Raya - Tangerang, Banten 15710&#10;0811-9131-551&#10;salam@taqmir.com&#10;taqmir.com&#10;&#10;[Peta Lokasi](https://goo.gl/maps/xa3bzTsorKWkEHzG8)&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="03c6d06iu7n20v51hfh0vgichc" TEXT="pattern">
+        <node ID="5jnd6lrqgih1rp67b59i3i4q50" TEXT="kontak taqmir"/>
+        <node ID="71gp1cm0t6lenab6o9n7t3tc3t" TEXT="alamat taqmir"/>
+      </node>
+    </node>
+  </node>
+</map>
