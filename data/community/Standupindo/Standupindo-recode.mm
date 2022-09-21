e025aaed670945c940c51e95542fdcf0a7cf32c4
Carik Ecosystem Builder <carik.bot@gmail.com>
2022-09-21 20:47:29 +0700

    Ecosystem Builder: community.Standupindo

---
+++ data/community/Standupindo/Standupindo-recode.mm
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+  <node ID="b9aa22deba98b3b20c7ac8aca2" STYLE="bubble" TEXT="Standupindo">
+    <hook NAME="accessories/plugins/NodeNote.properties">
+      <text>
+                *Standupindo*[.](https://standupindo.id/wp-content/uploads/2022/03/sucindo-logo-1024x236.png)
+
+Komunitas Stand Up Comedy Indonesia, selanjutnya dikenal dengan nama Stand Up Indonesia atau Standupindo lebih singkatnya, adalah komunitas yang dibentuk untuk mengumpulkan orang-orang yang tertarik dan sama-sama ingin belajar tentang stand up comedy, yang pada dasarnya mengumpulkan bakat-bakat para pelawak tunggal atau komika di seluruh Indonesia
+
+https://standupindo.id
+            </text>
+    </hook>
+    <node FOLDER="true" ID="624a5ab8-e8c1-4d73-bd22-d59ba7a75861" POSITION="right" TEXT="🗓 Openmic">
+      <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="7gls24bdffbl382luoabb9fs7f" TEXT="https://ecosystem.carik.id/services/community/Standupindo/schedule/">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="0rl6ac7053sju1uv7q34mpi1jg" TEXT="pattern">
+        <node ID="31p71n4rnl5ecd34o47ri8965l" TEXT="(event|openmic) startupindo"/>
+      </node>
+    </node>
+    <node FOLDER="true" ID="42l5hmkf6h40aq0laeuqjvqim6" POSITION="right" TEXT="🏅 About">
+      <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="6pl8fv6r4qsohga07b1reqibng" TEXT="*Standupindo*[.](https://standupindo.id/wp-content/uploads/2022/03/sucindo-logo-1024x236.png)&#10;&#10;Komunitas Stand Up Comedy Indonesia, selanjutnya dikenal dengan nama Stand Up Indonesia atau Standupindo lebih singkatnya, adalah komunitas yang dibentuk untuk mengumpulkan orang-orang yang tertarik dan sama-sama ingin belajar tentang stand up comedy, yang pada dasarnya mengumpulkan bakat-bakat para pelawak tunggal atau komika di seluruh Indonesia&#10;&#10;SIlakan follow semua akun social media Standupindo link ada di bawah dan juga gabung newsletter kita untuk mendapatkan email seputar event terbaru kita.&#10;&#10;*Full Team Kabinet*&#10;&#10;Adjisdoaibu&#10;President&#10;&#10;Erwin&#10;Vice President&#10;&#10;Gilang Bhaskara&#10;Public Relations&#10;&#10;Rizky Ubaidillah&#10;Secretary&#10;&#10;M. Rizal&#10;Event Coordinator&#10;&#10;Sigit Sulistyo&#10;Minister Of FInance&#10;&#10;Bata Efendi&#10;Public Relations&#10;&#10;Ernest Prakasa&#10;Advisors&#10;&#10;&#10;Terima Kasih Banyak Ya Udah Main Kesini, Semoga Harimu Menyenangkan!&#10;https://standupindo.id&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="0caud0gfce0matcau449qbpsro" TEXT="Action Type">
+        <node FOLDER="true" ID="3mpor5v59i5q04uieat560qd32" TEXT="button"/>
+      </node>
+      <node ID="2a195jmdpvkfiolo6otdt6m26r" TEXT="Action">
+        <node FOLDER="true" ID="4p92p028264kl9gu2s6cr02rhl" TEXT="🗺 Map Komunitas">
+          <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="191kr7rjhbvacj2mfdsl1h18i5" TEXT="*Komunitas Standupindo*&#13;&#10;&#13;&#10;[Lihat Map Komunitas Standupindo di sini](https://standupindo.id/community/)&#13;&#10;&#13;&#10;Komunitasmu belum terdaftar? Atau ada data yg salah?&#13;&#10;[Laporin](https://standupindo.id/laporin)&#13;&#10;&#13;&#10;">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node ID="1eojr0vmi5jagbffouqokkfe15" TEXT="🗓 Openmic">
+          <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="6i9db4hhfh60v1ct3u4nqjsoe3" TEXT="https://ecosystem.carik.id/services/community/Standupindo/schedule/">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+      </node>
+      <node FOLDER="true" ID="197103l4mb76pbmq5dstaguv0c" TEXT="pattern">
+        <node ID="53ftlr9v2uc90ubtqn8d8fbm10" TEXT="tentang standupindo"/>
+        <node ID="4k0ch2cfvh91sp5teg0utcui6d" TEXT="tentang standup indo"/>
+      </node>
+    </node>
+    <node FOLDER="true" ID="5llsdi02pp5uh6bet3hugni1j0" POSITION="left" TEXT="📞 Kontak">
+      <node BACKGROUND_COLOR="#F2F2F2" ID="75ibljtea1f3nmp91ab8l4n7la" TEXT="*Kontak Standupindo*&#10;&#10;- Facebook, facebook.com/StandUpIndonesia&#10;- Twitter, twitter.com/standupindo&#10;- Instagram, instagram.com/standupindonesia&#10;- Youtube, youtube.com/channel/UCtzlnlsx8IC59zb_Pcd0s4Q/videos&#10;&#10;standupindo.id&#10;&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="51ckpdl7rf4gjo8h8avffh231t" TEXT="pattern">
+        <node ID="7ptdvc68nsok1klmvgj6skhpgp" TEXT="(kontak|alamat) (standupindo|standup indo)"/>
+      </node>
+    </node>
+  </node>
+</map>
