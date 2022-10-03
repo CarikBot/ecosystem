2e3bec81fb01e3059175a1fd801966c98ef9f735
Carik Ecosystem Builder <carik.bot@gmail.com>
2022-10-03 11:22:32 +0700

    Ecosystem Builder: main.CarikBot,other.AgileManifesto,community.idBigData,community.Community,community.ProgrammerSemarang,community.Standupindo

---
+++ data/community/idBigData/idBigData-recode.mm
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+  <node ID="b9aa22deba98b3b20c7ac8aca2" STYLE="bubble" TEXT="idBigData">
+    <hook NAME="accessories/plugins/NodeNote.properties">
+      <text>
+                *idBigData* adalah sebuah komunitas bagi penggiat, pengguna dan pelaku Big Data di Indonesia yang bersifat terbuka dan tidak berafiliasi terhadap produk tertentu. idBigData diresmikan pada tanggal 4 Desember 2014 dalam acara Konferensi Big Data Indonesia yang berlangsung di Jogjakarta.
+
+*idBigData* diharapkan dapat menjadi sebuah komunitas yang menyatukan berbagai komponen yang berkepentingan dengan penguasaan dan penerapan teknologi Big Data di Indonesia, sehingga dapat terwujud sinergi yang membuahkan inovasi-inovasi yang bermanfaat untuk anggotanya dan juga bagi bangsa Indonesia.
+
+idBigData memiliki tujuan dan semangat :
+- Connect
+- Collaborate
+- Innovate
+
+[idBigData](https://idbigdata.com/)
+            </text>
+    </hook>
+    <node ID="193b56735e689ae86a01d91513" POSITION="right" TEXT="Konferensi KBI2022">
+      <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="61lrimrpaj3e6ejcmn8shl9n9u" TEXT="*Konferensi Big Data Indonesia 2022*[.](https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20220816043906_62fabcfa9462d.jpg)&#10;&#10;Mari bergabung dalam Konferensi virtual Big Data terbesar dihadiri oleh penggiat, pengguna, serta peminat teknologi Big Data dan Artificial Intelligence di Indonesia baik itu riset, implementasi, maupun pengembangannya.&#10;&#10;Konferensi Big Data Indonesia 2022 terbuka bagi seluruh penggiat, pengguna, serta peminat teknologi Big Data dan Artificial Intelligence di Indonesia baik itu riset, implementasi, maupun pengembangannya. Apakah anda termasuk di dalamnya?&#10;&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node FOLDER="true" ID="3reb0erfbilafq541nj6p40d7m" TEXT="Action Type">
+        <node FOLDER="true" ID="0ho41iueqjlp6pnemm7h4f0mlr" TEXT="button"/>
+      </node>
+      <node ID="0hl21dqtlfu5dd6o6ak7lb2226" TEXT="Action">
+        <node ID="1od2v9d0oub5inp4bjgstj17om" TEXT="🗓 Daftar Event">
+          <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="6gle3a0sgaqru76eupgpas8vo3" TEXT="*Daftar Event*&#13;&#10;&#13;&#10;Mari bergabung dalam Konferensi virtual Big Data terbesar dihadiri oleh penggiat, pengguna, serta peminat teknologi Big Data dan Artificial Intelligence di Indonesia baik itu riset, implementasi, maupun pengembangannya.&#13;&#10;&#13;&#10;...&#13;&#10;...&#13;&#10;....&#13;&#10;[Registrasi](https://www.loket.com/event/konferensi-big-data-indonesia-2022-virtual-conference)">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node FOLDER="true" ID="0u02ifid2cahj09qo5qtl2c4on" TEXT="🏍 Pembicara">
+          <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="6edrqupcdjp0j8353uiio4f98o" TEXT="*Pembicara KBI2022*&#10;&#10;Prof. Marcello Bonsangue&#10;Professor Computer Science - Leiden University&#10;&#10;Prof. Koji Nakao&#10;Guest Professor - Yokohama National University&#10;&#10;Prof. Dr. Eng. Wisnu Jatmiko, S.T., M.Kom.&#10;Professor Computer Science&#10;&#10;Beno K Pradekso&#10;CEO - Solusi247&#10;&#10;Dr. Anto Satriyo Nugroho&#10;Badan Riset dan Inovasi Nasional&#10;&#10;Prof. Setia Pramana&#10;Badan Pusat Statistik&#10;&#10;Dr. Budi Rahardjo&#10;Institut Teknologi Bandung&#10;&#10;Dios Kurniawan&#10;VP Data Governance &amp; Tech Compliance - Telkomsel&#10;&#10;Muhammad Maliqi Akbar&#10;Cloud Solution Architect Data &amp; AI - Microsoft Indonesia&#10;&#10;Deni Yulian&#10;Labs247&#10;">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node ID="0mbbnon4u065m0brlne9rmoh77" TEXT="Acara">
+          <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="3d2t41ound8tkqsjjdj32qa3np" TEXT="*Daftar Acara*&#13;&#10;&#13;&#10;....&#13;&#10;...&#13;&#10;...">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+        <node ID="72g4o644r3sbv943ktfb9fg56q" TEXT="Mitra dan Sponsor">
+          <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="69deslc8kkua7sh2o24fjfau56" TEXT="*Mitra dan Sponsor*&#13;&#10;&#13;&#10;berminat jadi mitra atau sponsor?">
+            <font NAME="SansSerif" SIZE="10"/>
+          </node>
+        </node>
+      </node>
+      <node ID="7lqrbvpnptt1ivqpliipeg1mcq" TEXT="pattern">
+        <node ID="05rl5vph5r7jki0gqfgde22nu0" TEXT="(konferensi|konverensi) (bigdata|idbigdata)"/>
+      </node>
+    </node>
+    <node ID="624a5ab8-e8c1-4d73-bd22-d59ba7a75861" POSITION="right" TEXT="📝 Resources">
+      <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="7gls24bdffbl382luoabb9fs7f" TEXT="*University Research*&#13;&#10;&#13;&#10;Peranan Universitas merupakan bagian penting dalam pengembangan BigData di Indonesia berikut beberapa riset yang telah dilakukan beberapa Universitas di Indonesia&#13;&#10;&#13;&#10;bla bla bla...">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="0rl6ac7053sju1uv7q34mpi1jg" TEXT="pattern">
+        <node ID="31p71n4rnl5ecd34o47ri8965l" TEXT="idbigdata resource"/>
+      </node>
+    </node>
+    <node ID="42l5hmkf6h40aq0laeuqjvqim6" POSITION="left" TEXT="🏅 About">
+      <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="6pl8fv6r4qsohga07b1reqibng" TEXT="*idBigData* adalah sebuah komunitas bagi penggiat, pengguna dan pelaku Big Data di Indonesia yang bersifat terbuka dan tidak berafiliasi terhadap produk tertentu. idBigData diresmikan pada tanggal 4 Desember 2014 dalam acara Konferensi Big Data Indonesia yang berlangsung di Jogjakarta.&#10;&#10;*idBigData* diharapkan dapat menjadi sebuah komunitas yang menyatukan berbagai komponen yang berkepentingan dengan penguasaan dan penerapan teknologi Big Data di Indonesia, sehingga dapat terwujud sinergi yang membuahkan inovasi-inovasi yang bermanfaat untuk anggotanya dan juga bagi bangsa Indonesia.&#10;&#10;idBigData memiliki tujuan dan semangat :&#10;- Connect&#10;- Collaborate&#10;- Innovate&#10;&#10;[idBigData](https://idbigdata.com/)&#10;">
+        <font NAME="SansSerif" SIZE="10"/>
+      </node>
+      <node ID="197103l4mb76pbmq5dstaguv0c" TEXT="pattern">
+        <node ID="53ftlr9v2uc90ubtqn8d8fbm10" TEXT="about idbigdata"/>
+      </node>
+    </node>
+  </node>
+</map>
