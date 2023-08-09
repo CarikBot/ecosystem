c278a47a6007ac9799b48c757762f6a7ce078354
Luri Darmawan <luri@kioss.com>
2023-08-09 09:29:46 +0700

    CBT-64 As an user, I want to know if the number calling me is a scammer or not

---
+++ source/services/other/telepon.net/CarikTeleponNet_lib.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Telepon.net handler
+ * 
+ * USAGE
+ *   [x] Check Telepon
+ *     $TN = new Carik\TeleponNet;
+ *     $TN->Check('+6234567890');
+ * 
+ *
+ * @date       09-08-2023 03:01
+ * @category   Library
+ * @package    TeleponNet
+ * @subpackage
+ * @copyright  Copyright (c) 2013-endless AksiIDE
+ * @license
+ * @version    0.0.1
+ * @link       https://carik.id
+ * @since
+ */
+
+namespace Carik;
+
+require_once "../../lib/simplehtmldom_2_0/simple_html_dom.php";
+
+class TeleponNet
+{
+  const BASE_URL = "https://telepon.net/nomortelepon/";
+
+  public function __construct(){
+  }
+
+  public function __get($property) {
+    if (property_exists($this, $property)) return $this->$property;
+  }
+
+  public function __set($property, $value) {
+    if (property_exists($this, $property)) $this->$property = $value;
+    if ($property=='DB') {
+      //$this->Cart->DB = $value;
+    }
+    return $this;
+  }
+
+  public function Check($APhone){
+    $phone = str_replace("+62", "", $APhone);
+    if (substr($phone, 0, 1) == "0") $phone = substr($phone, 1);
+    $url = TeleponNet::BASE_URL . $phone . '/';
+    //$html = file_get_contents("x.html");
+    //$html = @file_get_contents($url);
+
+    $html = file_get_html($url);
+    if (empty($html)) return false;
+    if (isStringExist("That page can’t be found", $html)) return false;
+
+    $estimation = $html->find("div.entry-predict");
+    $estimation = trim(strip_tags(@$estimation[0]->innertext));
+    $estimation = str_replace("  ", ": ", $estimation);
+    $return['estimation'] = $estimation;
+
+    $highlight = $html->find("div.post-highlight-circle");
+    $rate = trim(strip_tags(@$highlight[0]->innertext));
+    $report = trim(strip_tags(@$highlight[1]->innertext));
+    $return['rate'] = $rate;
+    $return['report'] = $report;
+    return $return;
+  }
+
+}
---
+++ source/services/other/telepon.net/check.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Check nomor telepon
+ * 
+ * USAGE
+ *   curl "http://ecosystem.carik.test/services/other/telepon.net/check/" -d "{yourpayload}"
+ * 
+ * @date       09-08-2024 01:05
+ * @category   other
+ * @package    telepon.net
+ * @subpackage
+ * @copyright  Copyright (c) 2013-endless AksiIDE
+ * @license
+ * @version    0.0.1
+ * @link       http://www.aksiide.com
+ * @since
+ */
+
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+date_default_timezone_set("Asia/Jakarta");
+require_once "../../lib/lib.php";
+require_once "../../config.php";
+require_once "CarikTeleponNet_lib.php";
+
+const JENIS_KELAMIN = ['', 'Laki-laki', 'Perempuan'];
+
+$UserId = @$RequestContentAsJson['data']['user_id'];
+$FullName = @$RequestContentAsJson['data']['FullName'];
+$Phone = @$RequestContentAsJson['data']['phone'];
+$Date = date("Y-m-d H:i:s");
+$DateAsInteger = strtotime($Date);
+
+function findPhoneInfo($APhone){
+  $TN = new Carik\TeleponNet;
+  $result = $TN->Check($APhone);
+  if ($result==false) return "Maaf, informasi nomor *$APhone* tidak ditemukan.";
+  $text = "Ditemukan informasi tentang $APhone berikut:";
+  $text .= "\n$result[estimation]";
+  $text .= "\nPenilaian: $result[rate]";
+  $text .= "\nJumlah laporan: $result[report]";
+  return $text;
+}
+
+//$Phone = '02180511793';
+if (!empty($Phone)){
+  $Text = findPhoneInfo($Phone);
+  RichOutput(0, $Text);
+}
+
+
+if ('CANCEL' == @$RequestContentAsJson['data']['submit']){
+  Output(0, "Pengisian form telah dibatalkan.\nTerima kasih.");
+}
+
+if ('OK' != @$RequestContentAsJson['data']['submit']){
+  // Build your question here
+  $Text = "*Cek nomor telepon*";
+  $Text .= "\nSilakan isi informasi ini.";
+  $Text .= "\n";
+
+  // general question
+  $generalQuestion[] = AddQuestion('string', 'phone', "Nomor telepon");
+  
+  $questionData[] = $generalQuestion;
+
+  $url = GetCurrentURL();
+  OutputQuestion( $Text, $questionData, $url, 'Phone Chek Form');
+}
+
+// Processing Data
+$Data = $RequestContentAsJson['data'];
+$Phone = strtoupper($Data['phone']);
+$Text = findPhoneInfo($Phone);
+RichOutput(0, $Text);
