124b7de021d882b7bc25846e2790af05f71a4f08
Luri Darmawan <luri@carik.id>
2023-09-22 14:57:43 +0700

    KPBI: astralaga 2023

---
+++ data/partner/KPBI/Astralaga.mm
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+    <node ID="b9aa22deba98b3b20c7ac8aca2" STYLE="bubble" TEXT="Astralaga">
+        <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                *ASTRALAGA 2023*
+Lomba Panahan yang Tak Boleh Dilewatkan!
+
+ihasa.id/astralaga-2023
+            </text>
+        </hook>
+        <node FOLDER="true" ID="4bfdqufd6kbcg9pgu6vi881gli" POSITION="right" TEXT="&#128467; Tanggal dan Lokasi">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="3nv6d72om8qt982tjgntk4qop1" TEXT="*Pelaksanaan ASTRALAGA*&#10;&#10;Acara ini akan berlangsung,&#10;Hari, tanggal: Sabtu, 21 Oktober 2023 &#10;Lokasi: Lapangan Kenari, Yogyakarta.&#10;&#10;[Map](https://maps.app.goo.gl/2Mt3cdtrYTg42aD79?g_st=iw)">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node ID="2bj1himu7ib558l6v5qo00rq53" TEXT="pattern">
+                <node FOLDER="true" ID="3s3tb6tbou3p947eb3j55r6jss" TEXT="jadwal astralaga"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="2l2bassdh884chslerrto9hr2m" POSITION="right" TEXT="&#127942; Hadiah">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="3a30i4epapg888r0p06vl9tl7o" TEXT="*Hadiah ASTRALAGA*&#10;&#10;Hadiah utama: Satu Paket Umroh.&#10;Beserta hadiah lain puluhan juta rupiah.&#10;">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node ID="3ffcmi074hv51quo0t0pv4qbo2" TEXT="pattern">
+                <node FOLDER="true" ID="5ug1jvau2s54mtrj5vm1gfv8jj" TEXT="hadiah astralaga"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="4phh2q0rdh4phea0001s8vohaj" POSITION="right" TEXT="✍️ Pendaftaran">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="56pkk3cu1e452qg1eu270ejk5l" TEXT="*Pendaftaran ASTRALAGA*&#10;&#10;Pendaftaran bisa dilakukan secara online melalu [tautan berikut](https://bit.ly/AstralagaBOT2023)&#10;">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node ID="7mq15f3k07sghd8r12rjg8fggt" TEXT="pattern">
+                <node FOLDER="true" ID="74luuqvemrui44qgn2m6571jdd" TEXT="pendaftaran astralaga"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="34ihd7m4mjgb7e4st2i5jbae8m" POSITION="right" TEXT="&#128216; Technical Hand Book">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="7it6nmpa5k6ujjisg5sd7coh2r" TEXT="*Technical Hand Book (THB)*&#10;Astralaga BOT 2023&#10;Lapangan Kenari Yogyakarta, 21 Oktober 2023&#10;&#10;&#10;Dokumen lengkap bisa diunduh dari [tautan berikut](https://ihasa.id/files/astralaga-thb.pdf)&#10;&#10;">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node FOLDER="true" ID="2egllav7gcei0kpi9q3b4h9b9a" TEXT="Action Type">
+                <node FOLDER="true" ID="5c0jbhqeil2i24n7jrc3mcjrgq" TEXT="button"/>
+            </node>
+            <node ID="2tp7rga4edsnc8knkjtldq3krq" TEXT="Action">
+                <node FOLDER="true" ID="121pb2i3nsqv72jhd1j5fnpfqj" TEXT="Peraturan Umum">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="4jeoas5ofqa5s9ha7krmsmimkk" TEXT="*Peraturan Umum Astralaga 2023*&#10;&#10;*Ketentuan Peserta*&#10;1. Sehat jasmani rohani.&#10;2. Tertulis di dalam daftar peserta.&#10;3. Mewakili club atau perseorangan.&#10;4. Wajib memakai pakaian tradisional meriah (dilarang menggunakan Jersey atau kaos Klub).&#10;5. Bersepatu.&#10;">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="499nkcknksq43m34aqgm4g8nn1" TEXT="pattern">
+                        <node FOLDER="true" ID="64v7l7uh3aa3l4fqo12cusfm5p" TEXT="(peraturan|aturan) umum"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="71cmhqkgcabtoausop4sesv2su" TEXT="Ketentuan Peralatan dan Teknik">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="05vaorcekpn3t2da17vikautra" TEXT="*Ketentuan Peralatan Astralaga 2023*&#10;&#10;*Busur*&#10;1. Menggunakan busur tradisional jenis horsebow dengan bahan fiber, laminasi atau hornbow.&#10;2. Busur tidak boleh diberi tanda apapun sebagai penanda jarak, jika ada penanda maka akan diisolasi/dilakban oleh panitia.&#10;3. Busur tidak boleh menggunakan window (jendela) dan arrow rest.&#10;&#10;*Nabl / Anak panah*&#10;1. Khosyabah/shaft natural: kayu atau bambu.&#10;2. Bahan fuq (Nock) natural (tanduk, kayu, tulang) atau self nock.&#10;3. Risy /vanes wajib pakai bulu.&#10;4. Jumlah bebas.&#10;&#10;*Kinanah/Quiver*&#10;1. Bebas&#10;&#10;*Teknik*&#10;1. Teknik Fast Shooting: Bebas&#10;2. Teknik Hadafiyah: Bebas&#10;&#10;*Daftar Istilah*&#10;Wirastra: Pemanah&#10;Nabl: Anak Panah&#10;Khosyabah: shaft&#10;Risy: bulu nabl&#10;Fuq: Nock&#10;Nashl: Mata panah&#10;Kinanah: Quiver&#10;Drel: Fast shooting dengan nabl dari kinanah&#10;Pamot: Fast shooting dengan nabl dari tangan&#10;Hadafiyah: target ground&#10;">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="38dd4iapa3j0rejf4fjs4c7lvj" TEXT="pattern">
+                        <node FOLDER="true" ID="6662im4at312lut75kkk8qg46g" TEXT="ketentuan (peralatan|alat)"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="24a35qjnhuddaij6g10vgkpmq9" TEXT="Teknis Pertandingan">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="1t3tp7jk9t5gukfrn0q97jfh7c" TEXT="*TEKNIS PERTANDINGAN ASTRALAGA BOT 2023*&#10;&#10;*TAHAP 1 FAST SHOOTING*&#10;&#10;Terdiri atas 5 tantangan fast shooting&#10;&#10;*Peraturan Umum*&#10;1. Setiap tantangan 2 rambahan.&#10;2. Jarak target 25 meter (pria) dan 20 meter (wanita).&#10;3. Target Mumtaz KPBI lingkaran biru, diameter 100 cm.&#10;4. Sistem penilaian hit n miss (kena 1, miss 0).&#10;5. Jumlah nabl bebas.&#10;6. Nabl yang jatuh ke tanah, tidak boleh digunakan (dipanahkan).&#10;7. Bentuk dan desain kinanah bebas.&#10;8. Aturan ini berlaku untuk pemanah dengan tangan kanan. Untuk pemanah kidal, berlaku kebalikannya.&#10;&#10;*Diskualifikasi*&#10;1. Mengambil nabl selain di reload area (A).&#10;2. Wirastra tidak memanahkan nabl nya di shooting line (B).&#10;3. Memanahkan nabl setelah bel waktu habis.&#10;&#10;```A -------------------- B ---------------------------- C&#10;         (20 m)            (Pria 25 m, Wanita 20 m)&#10;```&#10;&#10;Keterangan:&#10;A : Nabl area (reload area)&#10;B : Shooting line&#10;C : Bantalan target&#10;&#10;*Tantangan ke-1 - DREL STATIS*&#10;1. Fast Shooting di shooting line (B) dengan nabl dari kinanah (quiver).&#10;2. Nabl dipanahkan satu per satu.&#10;3. Waktu 60 detik.&#10;&#10;*Tantangan ke-2 - DREL DINAMIS*&#10;1. Wirastra berdiri di nabl area (A).&#10;2. Wirastra memasukkan 10 nabl ke dalam kinanah-nya dan meletakkan sisa nabl di nabl area (A).&#10;3. BEL BERBUNYI.&#10;4. Wirastra lari dari nabl area {A} sejauh 20 meter ke Shooting line (B).&#10;5. Wirastra memanahkan seluruh nabl yang ada di kinanah {quiver} satu per satu dari shooting line (B) ke target (C).&#10;6. Setelah nabl di kinanah habis, Wirastra lari ke nabl area (A) untuk mengambil nabl dan memasukkannya ke kinanah (Jumlah nabl bebas).&#10;7. Wirastra lari ke shooting line (B) dan memanahkan nabl nya satu per satu sampai waktu habis.&#10;8. Waktu 120 detik (dari bel berbunyi).&#10;&#10;*Tantangan ke 3 - PAMOT KANAN STATIS*&#10;1. Wirastra berdiri di shooting line (B) tanpa menggunakan kinanah.&#10;2. Nabl ditaruh 1 m dibelakang shooting line (B).&#10;3. BEL BERBUNYI.&#10;4. Wirasta mengambil nabl dengan tangan kanan dengan jumlah nabl bebas.&#10;5. Nabl dipanahkan dari shooting line (B) ke target (C) satu per satu.&#10;6. Setelah nabl habis, boleh mengambil nabl yang diletakkan 1 m di belakang shooting line (B).&#10;7. Waktu 90 detik.&#10;8. Nabl harus digenggam di tangan kanan, tidak boleh diletakkan tanah, tangan kiri, dll.&#10;&#10;*Tantangan ke 4 - PAMOT KANAN DINAMIS*&#10;1. Wirastra berdiri di nabl area (A).&#10;2. Wirastra menggenggam nabl di tangan kanan / draw hand dan meletakkan sisa nabl di tanah (nabl area). Jumlah nabl yang digenggam maksimal 10 nabl.&#10;3. BEL BERBUNYI.&#10;4. Wirastra lari dari nabl area {A} sejauh 20 meter ke Shooting line {B}.&#10;5. Wirastra memanahkan seluruh nabl satu per satu yang ada di tangan kanan / draw hand dari shooting line {B} ke target {C}.&#10;6. Setelah nabl di tangan habis, Wirastra lari ke nabl area {A} untuk mengambil nabl 1 genggam (jumlah bebas).&#10;7. Wirastra lari ke shooting line {B} dan memanahkan nabl nya satu per satu.&#10;8. Jika masih ada waktu, Wirastra boleh kembali lagi ke nabl area {A} untuk mengambil nabl lagi, dst. sampai waktu habis.&#10;9. Wirastra yang membawa nabl selain di tangan kanan / draw hand, didiskualifikasi.&#10;10. Waktu 120 detik.&#10;11. Nabl harus digenggam di tangan kanan, tidak boleh diletakkan tanah, tangan kiri, dll.&#10;&#10;*Tantangan ke 5 - PAMOT KIRI DINAMIS*&#10;1. Wirastra berdiri di nabl area {A}.&#10;2. Wirastra menggenggam nabl di tangan kiri / draw hand dan meletakkan sisa nabl di tanah (nabl area). Jumlah nabl yang digenggam maksimal 10 nabl.&#10;3. BEL BERBUNYI.&#10;4. Wirastra lari dari nabl area {A} sejauh 20 meter ke Shooting line {B}.&#10;5. Wirastra memanahkan seluruh nabl satu per satu yang ada di tangan kiri / bow hand/grip dari shooting line {B} ke target {C}.&#10;6. Setelah nabl di tangan habis, Wirastra lari ke nabl area {A} untuk mengambil nabl 1 genggam (jumlah bebas).&#10;7. Wirastra lari ke shooting line {B} dan memanahkan nabl nya satu per satu.&#10;8. Jika masih ada waktu, Wirastra boleh kembali lagi ke nabl area {A} untuk mengambil nabl lagi, dst. sampai waktu habis.&#10;9. Wirastra yang membawa nabl selain di tangan kiri / bow hand / grip, didiskualifikasi.&#10;10. Waktu 120 detik.&#10;11. Nabl harus digenggam di tangan kiri, tidak boleh diletakkan tanah, tangan kanan, dll.&#10;&#10;&#10;*TAHAP 2 HADAFIYAH*&#10;&#10;*Teknis lomba:*&#10;1. Jarak target 70 meter&#10;2. 9 nabl&#10;3. Face target puta&#10;4. 5 rambahan&#10;5. Sistem penilaian hit n miss&#10;6. Kaki dibelakang garis shooting line&#10;7. Waktu 180 detik&#10;&#10;*TAHAP 3 FINAL*&#10;&#10;Peserta babak final adalah wirastra yang masuk 32 besar dengan nilai tinggi di Tahap 1 Fast&#10;Shooting dan Tahap 2 Hadafiyah dengan rumus = (nilai tahap 1 Fast Shoot : 4) + nilai tahap 2&#10;Hadafiyah.&#10;&#10;Jika ada wirastra dengan nilai yang sama di peringkat 32 besar, maka akan dilakukan babak aduan&#10;1 rambahan 9 nabl. Jika hasil seri, akan dilakukan babak sudden death 1 nabl.&#10;&#10;*Teknis lomba:*&#10;1. Jarak target 90 meter.&#10;2. 9 nabl.&#10;3. Face target KALACAKRA SURYENGLAGA, diameter 100 cm.&#10;4. Sistem penilaian hit n miss.&#10;5. Waktu 180 detik.&#10;6. Dari 32 wirastra diambil 16 wirastra dengan nilai tertinggi.&#10;7. Dari 16 wirastra diambil 8 wirastra dengan nilai tertinggi.&#10;8. Dari 8 wirastra diambil kembali 4 wirastra dengan nilai tertinggi.&#10;9. 4 wirastra akan dilakukan babak aduan. Peringkat 1 vs peringkat 4 dan peringkat 2 vs peringkat 3.&#10;10. Perebutan peringkat 3 adalah wirastra yang kalah lawan wirastra yang kalah.&#10;11. Perebutan peringkat 1 dan 2 ditentukan dari wirastra yang menang.&#10;12. Setiap babak dilakukan 2 rambahan.&#10;">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                    <node ID="5sho7cde9bbev1q6v7cjr39sk2" TEXT="pattern">
+                        <node FOLDER="true" ID="1hbfm9lutnv01ge9nrb9kh0br5" TEXT="(teknis|teknik) pertandingan"/>
+                    </node>
+                </node>
+            </node>
+            <node ID="2dee7iat83950t2dt5f2i2q8l9" TEXT="pattern">
+                <node FOLDER="true" ID="2beiutqci2eqneoafgqtqq026v" TEXT="thb"/>
+                <node ID="26cn40n965l711pk8msuoov99c" TEXT="(book|thb) astralaga"/>
+                <node ID="74u1kbieobck57pd9f7epa5p8t" TEXT="(handbook|hand book) astralaga"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="2b6hhpuke9votck6bi8n6g0u3h" POSITION="left" TEXT="Live Scoring">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="0gr94th5oo6c3sce5vu1qpesru" TEXT="*LIVE SCORING ASTRALAGA*&#10;&#10;Live scoring event Astralaga saat ini belum tersedia.&#10;">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node ID="1pp3u3ckl1592ulam5nib4kdoc" TEXT="pattern">
+                <node FOLDER="true" ID="3mpnb7luup633g1s4sab7hfser" TEXT="scoring astralaga"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="1d0oeqhd0ct5lk8gppnkqu1ou4" POSITION="left" TEXT="Tip dan Trik">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="0fui5n85clu6je8qlcpgufsqno" TEXT="*Tips dan Trik Astralaga 2023*&#10;&#10;- video #1&#10;- video #2&#10;- video #3&#10;..&#10;..&#10;.&#10;">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node ID="38bhsud3m2ofra2v4iatlcva14" TEXT="pattern">
+                <node FOLDER="true" ID="0nn8lv2j8n3hnq9iv84duh0oci" TEXT="(tips|trik) astralaga"/>
+            </node>
+        </node>
+        <node FOLDER="true" ID="193b56735e689ae86a01d91513" POSITION="left" TEXT="Tentang Astralaga">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="61lrimrpaj3e6ejcmn8shl9n9u" TEXT="*ASTRALAGA 2023*&#10;*Lomba Panahan yang Tak Boleh Dilewatkan!*&#10;&#10;Dalam semangat kebersamaan dan persaudaraan, KPBI (Perkumpulan Panahan Berkuda Indonesia) kembali menggelar lomba panahan yang penuh tantangan. Tahun ini, para pecinta panahan dapat merasakan suasana baru yang dihadirkan dalam kompetisi ini.&#10;&#10;Setelah ditunggu-tunggu, ASTRALAGA kembali mengundang Anda untuk bergabung dalam kompetisi yang memacu adrenalin. Tantangan baru menanti untuk menguji keahlian memanah Anda. Jadi, siapkan panah terbaik Anda dan sambut keseruan lomba ini!&#10;&#10;Event ASTRALAGA, dengan tag line Brotherhood of Toxophilites.&#10;&#10;Acara ini akan berlangsung pada *21 Oktober 2023* di Lapangan Kenari, Yogyakarta. Lapangan Kenari akan menjadi saksi dari persaingan seru para pemanah Indonesia yang berkompetisi untuk memperebutkan gelar juara.&#10;&#10;Namun, bukan hanya gelar juara yang menarik para peserta, hadiah utama yang ditawarkan tak kalah menarik. Pemenang akan membawa pulang *Satu Paket Umroh*, membuka pintu petualangan spiritual yang tak terlupakan. Selain itu, hadiah-hadiah senilai puluhan juta rupiah juga disediakan untuk memenuhi kebahagiaan para pemenang.&#10;&#10;Tak hanya kompetisi, ASTRALAGA juga ingin memperkuat rasa persaudaraan di antara para penggemar panahan. Dengan tagar #solidarityforever, acara ini mengajak seluruh komunitas panahan untuk bersatu dan memperkokoh hubungan di antara mereka.&#10;&#10;Tertarik untuk ikut serta? Segera daftarkan diri Anda melalui [tautan pendaftaran ini](https://bit.ly/AstralagaBOT2023). Jangan lewatkan kesempatan untuk menjadi bagian dari perhelatan panahan tahunan yang selalu dinanti!&#10;&#10;Mari tunjukkan keterampilan panahan Anda, hadapi tantangan baru, dan raih kesempatan emas untuk membawa pulang hadiah utama yang mengagumkan. Ayo sambut ASTRALAGA 2023 dengan semangat dan kebersamaan yang tak terkalahkan!&#10;&#10;#Solidarity Forever&#10;#solidarity-forever&#10;">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node ID="31igbk16rdqgimdj9akg3caiod" TEXT="pattern">
+                <node FOLDER="true" ID="0pb40hr3440vfosp4jib0kogik" TEXT="(about|tentang) astralaga"/>
+            </node>
+        </node>
+    </node>
+</map>
