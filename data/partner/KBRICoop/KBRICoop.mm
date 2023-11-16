02e71ab94b6641ff4e1f3174436cb410a8b0acee
Luri Darmawan <luri@carik.id>
2023-11-16 11:00:21 +0700

    partner: KBRI Coop; docs: LLM Introduction

---
+++ data/partner/KBRICoop/KBRICoop.mm
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<map version="0.8.1">
+    <node ID="b9aa22deba98b3b20c7ac8aca2" STYLE="bubble" TEXT="KBRICoop">
+        <hook NAME="accessories/plugins/NodeNote.properties">
+            <text>
+                *KBRICoop*
+
+KBRICoop, berdiri sejak tahun ..... Kami merupakan s.......
+
+kbricoop.com
+            </text>
+        </hook>
+        <node ID="193b56735e689ae86a01d91513" POSITION="right" TEXT="&#128467; About">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="61lrimrpaj3e6ejcmn8shl9n9u" TEXT="*About KBRICoop*&#10;&#10;KBRICoop, berdiri sejak tahun ..... Kami merupakan s.......">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node FOLDER="true" ID="3ldt1ir22jmqmdqc4urtbdrp3k" TEXT="Action Type">
+                <node ID="0bn812f407silefphvvhnlcl0q" TEXT="button"/>
+            </node>
+            <node ID="7s4vo8n8lrn5dghlr7dp3h9r7q" TEXT="Action">
+                <node ID="28adnmtitjc9vpffpv8k802ede" TEXT="Visi Misi">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="38rpj300vmv36nsaoefl6r65et" TEXT="*JUDUL*&#10;&#10;Lorem ipsum, tak tak tung blah dung plak dung dung cess.">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                </node>
+                <node ID="4t3s35l0rd6bhqahgqjs8bkn80" TEXT="Struktur Organisasi">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="0l9s6j4m2lt94dchfdol053mlk" TEXT="*JUDUL*&#10;&#10;Lorem ipsum, tak tak tung blah dung plak dung dung cess.">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                </node>
+            </node>
+            <node ID="31igbk16rdqgimdj9akg3caiod" TEXT="pattern">
+                <node FOLDER="true" ID="0pb40hr3440vfosp4jib0kogik" TEXT="(about|tentang) kbri"/>
+            </node>
+        </node>
+        <node ID="624a5ab8-e8c1-4d73-bd22-d59ba7a75861" POSITION="right" TEXT="&#128029; Service">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="55k5a46vmbk928ppfo4vhhn2pr" TEXT="*KBRICoop Service*&#10;&#10;What We Do&#10;KBRICoop memiliki beberapa layanan, yaitu Ajaro ... ... ..">
+                <font NAME="SansSerif" SIZE="10"/>
+            </node>
+            <node FOLDER="true" ID="24au3hr7l8ki421gfj2frn3tpa" TEXT="Action Type">
+                <node FOLDER="true" ID="2inqavij1o2685mrdtgo1enp9o" TEXT="button"/>
+            </node>
+            <node ID="0584p8cnsragglitoardpb8t18" TEXT="Action">
+                <node ID="7a3sc6vdo7dppbuohigukq02da" TEXT="&#128682; Konsultasi">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="4pl5fsipmvo4vjs501rbea6r95" TEXT="*JUDUL*&#10;&#10;Lorem ipsum, tak tak tung blah dung plak dung dung cess.">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                </node>
+                <node ID="18loneg5b4isqjdv5i9akshfil" TEXT="&#127775; Talent Pool">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="3adhrbj7v998odn4udto1544bu" TEXT="*JUDUL*&#10;&#10;Lorem ipsum, tak tak tung blah dung plak dung dung cess.">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                </node>
+                <node ID="2ff1engvchn507mnifqo0l72ji" TEXT="&#129489;‍&#128187; Development">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="2dqbhpbvpeh9emnkdeol8fhkls" TEXT="*JUDUL*&#10;&#10;Lorem ipsum, tak tak tung blah dung plak dung dung cess.">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                </node>
+                <node ID="72o4bqqpr7k75l6u70mh5r15b9" TEXT="&#129489;‍&#128187; Investasi">
+                    <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="7dpl17f2a7lb42ktcg5bs2vlb6" TEXT="*JUDUL*&#10;&#10;Lorem ipsum, tak tak tung blah dung plak dung dung cess.">
+                        <font NAME="SansSerif" SIZE="10"/>
+                    </node>
+                </node>
+                <node FOLDER="true" ID="439ed0c9m2p1amnoau3nm255v7" TEXT="&#128222; Contact Us">
+                    <hook NAME="accessories/plugins/NodeNote.properties">
+                        <text>
+                            callback:contact kbri
+                        </text>
+                    </hook>
+                </node>
+            </node>
+            <node FOLDER="true" ID="4q7vbole2698m8turrvao8ctcq" TEXT="pattern">
+                <node FOLDER="true" ID="0tgnb1vhg8jie6in3ht2qbq6eg" TEXT="(service|layanan) kbri"/>
+            </node>
+        </node>
+        <node ID="67ddbcb1-85c9-4478-a0aa-580e9fdcd971" POSITION="left" TEXT="&#128222; Contact">
+            <node BACKGROUND_COLOR="#F2F2F2" FOLDER="true" ID="1m6eo1b0qeurhkrr3m8mf4ggan" TEXT="*Hubungi KBRICoop*[.](https://raw.githubusercontent.com/CarikBot/ecosystem/development/data/partner/KBRICoop/logo.png)&#10;&#10;Hubungi:&#10;Fitri, [whatsapp](wa.me/6285693041552)&#10;email: mail@kbricoop.com&#10;&#10;Sosial Media KBRICoop:&#10;- [Facebook](https://facebook.com/KBRICoop/)&#10;- [Instagram](https://instagram.com/KBRICoop/)"/>
+            <node FOLDER="true" ID="23m45snsmllgs3pnmpnsvu5rf3" TEXT="pattern">
+                <node FOLDER="true" ID="6vt3fgv7ihg1b5b7vlaua3pg6n" TEXT="(contact|kontak) kbri"/>
+                <node FOLDER="true" ID="4ilflt15tnjv0b1vcf28uf7q6k" TEXT="kbri (contact|kontak)"/>
+            </node>
+        </node>
+    </node>
+</map>
