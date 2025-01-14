a067b6a2ee73dd8adba9d8874684b7509155f40e
Luri Darmawan <luri@carik.id>
2025-01-14 09:49:15 +0700

    echo: remote post variable

--- source/services/tools/echo/json.php
+++ source/services/tools/echo/json.php
@@ -12,36 +12,40 @@ $headerList = array("cache-control", "User-Agent", "Accept", "Host", "accept-enc
 
 $output = "*JSON ECHO TESTER*";
 $output .= " -- ⭐️🌟✨🔆🔅🕶👓⭕️🚫⛔️📛💤";
+$output .= "\nMethod: " . $_SERVER['REQUEST_METHOD'];
 $output = $output . "\n";
-$output = $output . "\n*GET:*";
+$output = $output . "\n*Parameter:*";
 foreach ($_GET as $key => $value) {
-	$output = $output . "\n$key=$value";
+	$output = $output . "\n $key=$value";
 }
-$output = $output . "\n";
-$output = $output . "\n*POST:*";
-foreach ($_POST as $key => $value) {
-	if ('pattern'==$key) continue;
-	$value = urldecode( $value);
-	$output = $output . "\n$key=$value";
-}
+// $output = $output . "\n";
+// $output = $output . "\n*POST:*";
+// foreach ($_POST as $key => $value) {
+// 	if ('pattern'==$key) continue;
+// 	$value = urldecode( $value);
+// 	$output = $output . "\n$key=$value";
+// }
 
 $output = $output . "\n";
 $output = $output . "\n*HEADER:*";
 $headers = apache_request_headers();
 foreach ($headers as $key => $value) {
 	if (!in_array($key, $headerList))
-		$output = $output . "\n$key=$value";
+		$output = $output . "\n $key=$value";
 }
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 $output .= "\n\nUserAgent: " . $userAgent;
+if ($RequestContentAsJson['data']['channel_id'] == 'telegram'){
+  $output = str_replace("_","\_", $output);
+}
 
 // json code
 $jsonAsString = json_encode($RequestContentAsJson, JSON_PRETTY_PRINT);
 $output .= "\n\nRequest Content:\n";
 $output .= "```$jsonAsString```";
+$output .= "\n";
 $output .= "\n> Sebagian field mungkin sudah _deprecated_, silakan hubungi Tim Developer Carik";
 
-$output = str_replace("_","\_", $output);
 $RESULT['code'] = 0;
 $RESULT['get'] = $_GET;
 $RESULT['post'] = $_POST;
